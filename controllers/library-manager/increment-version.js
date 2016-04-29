0a50b9015cd5a1bba0ccd09a543361f24da0541f
Mr. Xyfir <mr@xyfir.com>
2016-04-29 13:33:36 -0700

    Modify to be called from library-manager.

---
+++ controllers/library-manager/increment-version.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const db = require("../../lib/db");
+
+/*
+    PUT api/library-manager/:server/:lib/books/:book
+    REQUIRED
+        type: string, freeSpace: number
+    RETURN
+        { error: boolean }
+    DESCRIPTION
+        Increments an ebook's metadata or cover version
+        Updates library server's free space
+*/
+module.exports = function(req, res) { 
+
+    const column = req.body.type == "metadata" ? "version_metadata" : "version_cover";
+    
+    let sql = `
+        SELECT user_id FROM users WHERE library_server_id = ? AND library_id = ?
+    `;
+    let vars = [req.params.server, req.params.lib];
+    
+    db(cn => cn.query(sql, (err, rows) => {
+        if (err || !rows.length) {
+            cn.release();
+            res.json({ error: true });
+        }
+        else {
+            const uid = rows[0].user_id;
+            
+            sql = `UPDATE books SET ${column} = ${column} + 1 WHERE user_id = ? AND book_id = ?`;
+            vars = [uid, req.params.book];
+            
+            cn.query(sql, vars, (err, result) => {
+                if (err || !result.affectedRows) {
+                    cn.release();
+                    res.json({ error: true });
+                }
+                else {
+                    sql = "UPDATE servers SET free_space = ? WHERE server_id = ?";
+                    vars = [req.params.server];
+                    
+                    cn.query(sql, vars, (err, result) => {
+                        cn.release();
+                        res.json({ error: false });
+                    });
+                }
+            });
+        }
+    }));
+    
+};
