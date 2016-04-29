cc01078b2f70ede5da4b5e3bb78dc6cef765a48b
Mr. Xyfir <mr@xyfir.com>
2016-04-29 11:33:33 -0700

    Add controller for DELETE api/books/:book/note

---
+++ controllers/books/note/remove.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const db = require("../../lib/db");
+
+/*
+    DELETE api/books/:book/note
+    REQUIRED
+        cfi: string
+    RETURN
+        { error: boolean }
+    DESCRIPTION
+        Delete note at body.cfi from :book
+*/
+module.exports = function(req, res) { 
+
+    const sql = `DELETE FROM notes WHERE user_id = ? AND book_id = ? AND cfi = ?`;
+    const vars = [req.session.uid, req.params.book, req.body.cfi];
+    
+    db(cn => cn.query(sql, vars, (err, result) => {
+        cn.release();
+        res.json({ error: !!err || !result.affectedRows });
+    }));
+    
+};
