41b76dd10c10d287d0cfca735adee63f63888fdb
Mr. Xyfir <mr@xyfir.com>
2016-04-29 11:29:43 -0700

    Add controller for DELETE api/books/:book/bookmark

---
+++ controllers/books/bookmark/remove.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const db = require("../../lib/db");
+
+/*
+    DELETE api/books/:book/bookmark
+    REQUIRED
+        cfi: string
+    RETURN
+        { error: boolean }
+    DESCRIPTION
+        Delete bookmark at body.cfi from :book
+*/
+module.exports = function(req, res) { 
+
+    const sql = `DELETE FROM bookmarks WHERE user_id = ? AND book_id = ? AND cfi = ?`;
+    const vars = [req.session.uid, req.params.book, req.body.cfi];
+    
+    db(cn => cn.query(sql, vars, (err, result) => {
+        cn.release();
+        res.json({ error: !!err || !result.affectedRows });
+    }));
+    
+};
