278e66514c434f4427ce106ecf51c7f2ce445ea2
Mr. Xyfir <mr@xyfir.com>
2016-05-02 09:34:16 -0700

    Implement express-session. Use config object

--- app.js
+++ app.js
@@ -4,12 +4,35 @@ const express = require("express");
 const parser = require("body-parser");
 const app = express();
 
-app.listen(process.env.port, () => {
-    console.log("SERVER RUNNING ON", process.env.port);
-});
+const sstore = require("express-mysql-session");
+const config = require("./config");
 
-app.use("/", express.static(__dirname + "/public"));
+/* Sessions */
+const sessionStore = new sstore({
+    host: config.database.mysql.host,
+    port: config.database.mysql.port,
+    user: config.database.mysql.user,
+    password: config.database.mysql.password,
+    database: config.database.mysql.database,
+    useConnectionPooling: true
+});
+app.use(session({
+    secret: config.secrets.session,
+    store: sessionStore,
+    saveUninitialized: true,
+    resave: true,
+    cookie: {
+        httpOnly: false
+    }
+}));
+
+/* Body Parser */
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
+app.use("/", express.static(__dirname + "/public"));
 app.use("/api", require("./controllers/"));
+
+app.listen(config.environment.port, () => {
+    console.log("SERVER RUNNING ON", config.environment.port);
+});
