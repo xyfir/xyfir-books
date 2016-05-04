10a2274698b920ffa5d30b1c73d7c47cace19c3f
Mr. Xyfir <mr@xyfir.com>
2016-05-04 09:56:51 -0700

    Handle setting subscription for expired subscriptions. Create new library if user's old library was deleted

--- controllers/account/purchase.js
+++ controllers/account/purchase.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const randString = require("randomstring");
+const request = require("request");
 const db = require("../../lib/db");
 
 /*
@@ -38,29 +40,99 @@ module.exports = function (req, res) {
         }
 
         let sql = `
-            SELECT subscription FROM users WHERE user_id = ?
+            SELECT subscription, library_id FROM users WHERE user_id = ?
         `;
         db(cn => cn.query(sql, [req.session.uid], (err, rows) => {
             if (err || !rows.length) {
                 cn.release();
                 res.json({ error: true, message: "An unknown error occured" });
+                return;
             }
-            else {
-                // Add months to current subscription expiration (or now())
-                let subscription = rows[0].subscription == 0
-                    ? (Date.now() + (subscriptions[req.body.subscription].days * 86400 * 1000))
-                    : (rows[0].subscription + (subscriptions[req.body.subscription].days * 86400 * 1000));
+            
+            // Add months to current subscription expiration (or now())
+            let subscription;
+            
+            // Set subscription expiration
+            if (Date.now() > rows[0].subscription)
+                subscription = Date.now() + (subscriptions[req.body.subscription].days * 86400 * 1000);
+            else
+                subscription = rows[0].subscription + (subscriptions[req.body.subscription].days * 86400 * 1000);
 
-                sql = `
-                    UPDATE users SET subscription = ? WHERE user_id = ?
-                `;
-                cn.query(sql, [subscription, req.session.uid], (err, result) => {
+            sql = `
+                UPDATE users SET subscription = ? WHERE user_id = ?
+            `;
+            cn.query(sql, [subscription, req.session.uid], (err, result) => {
+                if (err || !result.affectedRows) {
+                    cn.release();
+                    res.json({ error: true, message: "Contact support" });
+                }
+                else if (rows[0].library_id == '') {
+                    // We need to create a library for user, find library-manager server
+                    sql = `
+                        SELECT * FROM servers WHERE is_select = 0 AND space_free > 100000000 + (space_total * 0.1)
+                        AND library_limit > library_count LIMIT 1
+                    `;
+                    
+                    cn.query(sql, (err, rows) => {
+                        if (err || !rows.length) {
+                            cn.release();
+                            res.json({ error: true, message: "Contact support" });
+                            return;
+                        }
+                        
+                        const library = req.session.uid + '-' + randString.generate(40);
+                        
+                        // Update user's library / library server id
+                        sql = "UPDATE users SET library_id = ?, library_server_id = ? WHERE user_id = ?";
+                        let vars = [library, rows[0].server_id, req.session.uid];
+                        
+                        cn.query(sql, vars, (err, result) => {
+                            if (err || !result.affectedRows) {
+                                cn.release();
+                                res.json({ error: true, message: "Contact support" });
+                                return;
+                            }
+                            
+                            // Create library on server
+                            request.post(rows[0].address + "library/" + library, (err, response, body) => {
+                                if (err) {
+                                    cn.release();
+                                    res.json({ error: true, message: "Contact support" });
+                                    return;
+                                }
+                                
+                                body = JSON.parse(body);
+                                
+                                if (body.error) {
+                                    cn.release();
+                                    res.json({ error: true, message: "Contact support" });
+                                    return;
+                                }
+                                
+                                // Update server's library_count
+                                sql = `
+                                    UPDATE servers SET library_count = library_count + 1 WHERE server_id = ?    
+                                `;
+                                
+                                cn.query(sql, [rows[0].server_id], (err, result) => {
+                                    res.json({ error: false, message: "" });
+                                
+                                    req.session.library = {
+                                        id: library, address: rows[0].address, server: rows[0].server_id
+                                    };
+                                    req.session.subscription = subscription;
+                                });
+                            });
+                        });
+                    });
+                }
+                else {
                     cn.release();
                     
                     req.session.subscription = subscription;
                     res.json({ error: false, message: "" });
-                });
-            }
+                }
+            });
         }));
     });
 
