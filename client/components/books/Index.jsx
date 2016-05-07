10b1ac5f6ae81ddc29802ca44ee2dac80a423bc7
Mr. Xyfir <mr@xyfir.com>
2016-05-06 17:14:09 -0700

    Add Books index component

---
+++ client/components/books/Index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+// Components
+import MetaData from "./Metadata";
+import Upload from "./Upload";
+import Search from "./Search";
+import List from "./list/";
+import Read from "./read/";
+
+export default class Books extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        switch (this.props.data.view.split('/')[1]) {
+            case "METADATA":
+                return <MetaData data={this.props.data} dispatch={this.props.dispatch} />;
+            case "UPLOAD":
+                return <Upload data={this.props.data} dispatch={this.props.dispatch} />;
+            case "SEARCH":
+                return <Search data={this.props.data} dispatch={this.props.dispatch} />;
+            case "LIST":
+                return <List data={this.props.data} dispatch={this.props.dispatch} />;
+            case "READ":
+                return <Read data={this.props.data} dispatch={this.props.dispatch} />;
+        }
+    }
+
+}
