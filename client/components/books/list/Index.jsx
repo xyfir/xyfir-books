62a05a632dda43dd78ab6ebcb7bc929e8c072f27
Mr. Xyfir <mr@xyfir.com>
2016-05-07 17:04:53 -0700

    Remove "list by" components

--- client/components/books/list/Index.jsx
+++ client/components/books/list/Index.jsx
@@ -7,12 +7,6 @@ import { loadBooks } from "../../../actions/creators/books";
 import loadBooksFromApi from "../../../lib/books/load-from-api";
 
 // Components
-import ByAuthorSort from "./by/AuthorSort";
-import ByAuthors from "./by/Authors";
-import ByRating from "./by/Rating";
-import BySeries from "./by/Series";
-import ByTag from "./by/Tag";
-//
 import AuthorSort from "./group/AuthorSort";
 import Authors from "./group/Authors";
 import Ratings from "./group/Ratings";
@@ -44,37 +38,19 @@ export default class Books extends React.Component {
     }
 
     render() {
-        const view = this.props.data.view.split('/');
-        
-        if (view[2] == "BY") {
-            switch (this.props.data.view.split('/')[3]) {
-                case "TAG":
-                    return <ByTag data={this.props.data} dispatch={this.props.dispatch} />;
-                case "RATING":
-                    return <ByRating data={this.props.data} dispatch={this.props.dispatch} />;
-                case "SERIES":
-                    return <BySeries data={this.props.data} dispatch={this.props.dispatch} />;
-                case "AUTHORS":
-                    return <ByAuthors data={this.props.data} dispatch={this.props.dispatch} />;
-                case "AUTHOR_SORT":
-                    return <ByAuthorSort data={this.props.data} dispatch={this.props.dispatch} />;
-            }
-        }
-        else {
-            switch (this.props.data.view.split('/')[2]) {
-                case "ALL":
-                    return <All data={this.props.data} dispatch={this.props.dispatch} />;
-                case "TAGS":
-                    return <Tags data={this.props.data} dispatch={this.props.dispatch} />;
-                case "RATINGS":
-                    return <Ratings data={this.props.data} dispatch={this.props.dispatch} />;
-                case "SERIES":
-                    return <Series data={this.props.data} dispatch={this.props.dispatch} />;
-                case "AUTHORS":
-                    return <Authors data={this.props.data} dispatch={this.props.dispatch} />;
-                case "AUTHOR_SORT":
-                    return <AuthorSort data={this.props.data} dispatch={this.props.dispatch} />;
-            }
+        switch (this.props.data.view.split('/')[2]) {
+            case "ALL":
+                return <All data={this.props.data} dispatch={this.props.dispatch} />;
+            case "TAGS":
+                return <Tags data={this.props.data} dispatch={this.props.dispatch} />;
+            case "RATINGS":
+                return <Ratings data={this.props.data} dispatch={this.props.dispatch} />;
+            case "SERIES":
+                return <Series data={this.props.data} dispatch={this.props.dispatch} />;
+            case "AUTHORS":
+                return <Authors data={this.props.data} dispatch={this.props.dispatch} />;
+            case "AUTHOR_SORT":
+                return <AuthorSort data={this.props.data} dispatch={this.props.dispatch} />;
         }
     }
 
