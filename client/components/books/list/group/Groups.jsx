32052fdd2d93d8560c844b980067a965821782db
Mr. Xyfir <mr@xyfir.com>
2016-05-08 09:56:37 -0700

    Add component for listing list groups and the count of unique instances within each group

---
+++ client/components/books/list/group/Groups.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+export default class ListGroups extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const groups = [
+            { name: "All", property: "all", arr: [] },
+            { name: "Authors", property: "authors", arr: [] },
+            { name: "Author Sort", property: "author_sort", arr: [] },
+            { name: "Ratings", property: "rating", arr: [] },
+            { name: "Series", property: "series", arr: [] },
+            { name: "Tags", property: "tags", arr: [] }
+        ];
+        
+        // Count unique instances within each list group
+        groups.forEach((group, i) => {
+            // Count books for all
+            if (i == 0) {
+                groups[0].arr = this.props.data.books.map(b => b.id);
+                return;
+            }
+            
+            this.props.data.books.forEach(book => {
+                // Tags group
+                if (group.property == "tags") {
+                    this.props.data.books.forEach(book => {
+                        book.tags.forEach(tag => {
+                            if (groups[i].arr.indexOf(tag) == -1)
+                                groups[i].arr.push(tag);
+                        });
+                    });
+                }
+                // Non-tags groups
+                else {
+                    const val = group.property == "rating"
+                        ? Math.floor(book[group.poperty]) : book[group.poperty];
+                    
+                    if (groups[i].arr.indexOf(val) == -1)
+                        groups[i].arr.push(val);
+                }
+            });
+        });
+        
+        groups[1].property = "author-sort";
+        
+        return (
+            <div className="list-groups">
+                <table className="list">{
+                    groups.map(group => {
+                        return (
+                            <tr>
+                                <td><a href={`#books/list/${group.property}`}>{group.name}</a></td>
+                                <td>{group.arr.length}</td>
+                            </tr>
+                        )
+                    })
+                }</table>
+            </div>
+        );
+    }
+
+}
