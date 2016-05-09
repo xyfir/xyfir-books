f083befa2c4e374743acad529646bea33e551151
Mr. Xyfir <mr@xyfir.com>
2016-05-09 13:40:29 -0700

    Add module for cleaning a string for use in a url

---
+++ client/lib/url/clean.js
@@ -0,0 +1,9 @@
+export default function(input) {
+    
+    return input.replace(
+        new RegExp("[^0-9a-zA-Z ]", "g"), ""
+    ).replace(
+        new RegExp(" ", "g"), "-"
+    ).substr(0, 20);
+    
+}
