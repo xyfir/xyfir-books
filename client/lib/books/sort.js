900ed65198db76e652868c568a29de87b14109cd
Mr. Xyfir <mr@xyfir.com>
2016-05-09 10:51:47 -0700

    Add module for sorting books by a field / direction

---
+++ client/lib/books/sort.js
@@ -0,0 +1,34 @@
+// asc == true for A -> Z, 0 -> 9
+export default function(books, field, asc = true) {
+    
+    return books.sort((a, b) => {
+        if (a[field] < b[field]) {
+            return asc ? -1 : 1;
+        }
+        else if (a[field] > b[field]) {
+            return asc ? 1 : -1;
+        }
+        // If same series, sort by series_index
+        else if (field == "series") {
+            if (a.series_index < b.series_index)
+                return -1;
+            else if (a.series_index > b.series_index)
+                return 1;
+            else
+                return 0;
+        }
+        // If same authors, sort by title
+        else if (field == "authors" || field == "authors_sort") {
+            if (a.title < b.title)
+                return -1;
+            else if (a.title > b.title)
+                return 1;
+            else
+                return 0;
+        }
+        else {
+            return 0;
+        }
+    });
+    
+}
