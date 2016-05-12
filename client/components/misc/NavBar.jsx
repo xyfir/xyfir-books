95acb9418add73502cf384948bb4387a79e6b939
Mr. Xyfir <mr@xyfir.com>
2016-05-12 15:54:36 -0700

    Add NavBar component

---
+++ client/components/misc/NavBar.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+export default class NavBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+    
+    render() { 
+        const showText = window.screen.height < window.screen.width;
+        
+        return (
+            <nav className="nav-bar">
+                {this.props.back !== undefined ? (
+                    <a href={this.props.back}>
+                        <span
+                            className="icon-back"
+                            title="Back"
+                        />{showText ? " Back" : ""}
+                    </a>
+                ) : (<a />)}
+                
+                {this.props.home ? (
+                    <a href="#books/recently-opened">
+                        <span
+                            className="icon-home"
+                            title="Home / Recently Opened"
+                        />{showText ? " Home" : ""}
+                    </a>
+                ) : (<a />)}
+                
+                {this.props.account ? (
+                    <a href="#account">
+                        <span
+                            className="icon-user"
+                            title="Account"
+                        />{showText ? " Account" : ""}
+                    </a>
+                ) : (<a />)}
+                
+                <span className="title">{this.props.title}</span>
+                
+                {this.props.library ? (
+                    <a href="#library-manage/info">
+                        <span
+                            className="icon-book"
+                            title="Library"
+                        />{showText ? " Library" : ""}
+                    </a>
+                ) : (<a />)}
+                
+                {this.props.settings !== undefined ? (
+                    <a href={`#settings/${
+                        this.props.settings == "" ? "general" : this.props.settings
+                    }`}>
+                        <span
+                            className="icon-settings"
+                            title="Settings"
+                        />{showText ? " Settings" : ""}
+                    </a>
+                ) : (<a />)}
+                
+                {this.props.books ? (
+                    <a href="#books/list">
+                        <span
+                            className="icon-book-open"
+                            title="Book List"
+                        />{showText ? " Books" : ""}
+                    </a>
+                ) : (<a />)}
+            </nav>
+        );
+    }
+
+}
