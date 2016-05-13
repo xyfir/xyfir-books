cd23fb44191f9e354e352e54c56f08a7af1e5a93
Mr. Xyfir <mr@xyfir.com>
2016-05-13 10:20:55 -0700

    Add module for requesting from API a specified amount of free space on storage server

---
+++ client/lib/request/space-needed.js
@@ -0,0 +1,31 @@
+// Modules
+import request from "./index";
+
+// Constants
+import { URL } from "../../constants/config";
+
+// Action creators
+import { setLibraryAddress } from "../../actions/creators/account/";
+
+export default function (bytes, dispatch, fn) {
+    
+    ajax({
+        url: URL + "api/library-manager/space",
+        method: "PUT", data: {bytes},
+        success: (res) => {
+            if (res.error) {
+                fn(true);
+            }
+            else if (res.libraryServerAddress !== undefined) {
+                // Update state.account.library.address
+                dispatch(setLibraryAddress(res.libraryServerAddress));
+                
+                fn(false, res.libraryServerAddress);
+            }
+            else {
+                fn(false);
+            }
+        }
+    });
+    
+}
