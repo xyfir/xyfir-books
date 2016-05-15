30a81ac27ad6a929fa8b07667ada5c38d4b2ac20
Mr. Xyfir <mr@xyfir.com>
2016-05-15 15:41:36 -0700

    Add / change routes / views

--- client/lib/url/update-view.js
+++ client/lib/url/update-view.js
@@ -53,13 +53,29 @@ export default function(store) {
         }
         else {
             switch (hash[1]) {
-                case "metadata":
-                    return store.dispatch(changeView(VIEWS.EDIT_METADATA));
+                case "recently-opened":
+                    return store.dispatch(changeView(VIEWS.RECENTLY_OPENED));
+                case "add-format":
+                    return store.dispatch(changeView(VIEWS.ADD_FORMAT));
+                case "bulk-edit":
+                    return store.dispatch(changeView(VIEWS.BULK_EDIT));
+                case "manage":
+                    return store.dispatch(changeView(VIEWS.MANAGE_BOOK));
                 case "read":
                     return store.dispatch(changeView(VIEWS.READ_BOOK));
                 case "upload":
                     return store.dispatch(changeView(VIEWS.UPLOAD_BOOKS));
             }
+        }
+    }
+    else if (hash[0] == "settings") {
+        switch (hash[1]) {
+            case "book-list":
+                return store.dispatch(changeView(VIEWS.BOOK_LIST_SETTINGS));
+            case "general":
+                return store.dispatch(changeView(VIEWS.GENERAL));
+            case "reader":
+                return store.dispatch(changeView(VIEWS.READER));
         }
     }
     
