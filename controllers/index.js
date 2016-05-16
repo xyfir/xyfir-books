b64b630d252161941d553d9df64480ff417fb3c7
Mr. Xyfir <mr@xyfir.com>
2016-05-16 10:48:50 -0700

    Add / change book controllers

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,7 @@
 const router = require("express").Router();
 
 /* Library Manager */
+
 router.post("/library-manager/:server/:lib/library", require("./library-manager/add-library"));
 router.post("/library-manager/:server/:lib/books", require("./library-manager/add-books"));
 router.put("/library-manager/:server/:lib/books/:book", require("./library-manager/increment-version"));
@@ -10,18 +11,26 @@ router.post("/library-manager/:server/space", require("./library-manager/space-n
 router.put("/library-manager/:server/space", require("./library-manager/update-freespace"));
 
 /* Books */
+
 router.route("/books")
     .get(require("./books/list"))
     .delete(require("./books/delete"));
-router.get("/books/:book", require("./books/info"));
+
+router.get("/books/:book", require("./books/open"));
+router.post("/books/:book/close", require("./books/close"));
+
 router.route("/books/:book/bookmark")
     .post(require("./books/bookmark/add"))
     .delete(require("./books/bookmark/remove"));
+
 router.route("/books/:book/note")
     .post(require("./books/note/add"))
     .delete(require("./books/note/remove"));
+    
+router.put("/books/:book/word-count", require("./books/word-count"));
 
 /* Account */
+
 router.get("/account", require("./account/info"));
 router.put("/account/subscription", require("./account/purchase"));
 router.post("/account/login", require("./account/login"));
