f76ad26ba2e3f7755cc0dd695dd63dedfe282e25
Mr. Xyfir <mr@xyfir.com>
2016-05-17 15:00:25 -0700

    Add LibraryInfo component

---
+++ client/components/library/Info.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+// Modules
+import request from "../../lib/request/";
+
+// Components
+import NavBar from "../misc/NavBar";
+
+export default class LibraryInfo extends React.Component {
+
+    constructor(props) {
+        super(props);
+        
+        const url = this.props.data.account.library.address + "library/"
+            + this.props.data.account.library.id + "/size";
+            
+        this.state = { size: 0 };
+        
+        request({url, success: (res) => {
+            if (res.error)
+                this.setState({ size: -1 });
+            else
+                this.setState({ size: res.size });
+        }});
+    }
+
+    render() {
+        return (
+            <div className="library-info">
+                <NavBar
+                    home={true}
+                    account={true}
+                    title="Library - Info"
+                    settings={""}
+                    books={true}
+                />
+                
+                <section className="info">
+                    <h2>Info</h2>
+                    <p>
+                        This information details the size and book count of your library in the cloud. Locally stored size and book count may differ.
+                    </p>
+                    
+                    <table>
+                        <tr>
+                            <th>Books</th>
+                            <td>{this.props.data.books.length}</td>
+                        </tr>
+                        <tr>
+                            <th>Size</th>
+                            <td>{
+                                this.state.size == -1
+                                    ? "Could not calculate size"
+                                    : (this.state.size * 0.000001) + " MB"
+                            }</td>
+                        </tr>
+                    </table>
+                </section>
+                
+                <hr />
+                
+                <section className="buttons">
+                    <button
+                        className="btn-secondary"
+                        onClick={() => {
+                            location.hash = "library/download";
+                        }}
+                    ><span className="icon-download" />Download</button>
+                    <button
+                        className="btn-secondary"
+                        onClick={() => {
+                            location.hash = "library/upload";
+                        }}
+                    ><span className="icon-upload" />Upload</button>
+                </section>
+            </div>
+        );
+    }
+
+}
