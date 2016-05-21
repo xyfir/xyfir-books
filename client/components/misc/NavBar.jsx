c0ad6622425b06ef1affecc7c4c7fe789978549d
Mr. Xyfir <mr@xyfir.com>
2016-05-21 16:24:40 -0700

    Split navbar into left / title / right

--- client/components/misc/NavBar.jsx
+++ client/components/misc/NavBar.jsx
@@ -11,54 +11,58 @@ export default class NavBar extends React.Component {
         
         return (
             <nav className="nav-bar">
-                {this.props.home ? (
-                    <a href="#books/recently-opened">
-                        <span
-                            className="icon-home"
-                            title="Home / Recently Opened"
-                        />{showText ? " Home" : ""}
-                    </a>
-                ) : (<a />)}
-                
-                {this.props.account ? (
-                    <a href="#account">
-                        <span
-                            className="icon-user"
-                            title="Account"
-                        />{showText ? " Account" : ""}
-                    </a>
-                ) : (<a />)}
+                <div className="left">
+                    {this.props.home ? (
+                        <a href="#books/recently-opened">
+                            <span
+                                className="icon-home"
+                                title="Home / Recently Opened"
+                            />{showText ? " Home" : ""}
+                        </a>
+                    ) : (<a />)}
+                    
+                    {this.props.account ? (
+                        <a href="#account">
+                            <span
+                                className="icon-user"
+                                title="Account"
+                            />{showText ? " Account" : ""}
+                        </a>
+                    ) : (<a />)}
+                </div>
                 
                 <span className="title">{this.props.title}</span>
                 
-                {this.props.library ? (
-                    <a href="#library/info">
-                        <span
-                            className="icon-book"
-                            title="Library"
-                        />{showText ? " Library" : ""}
-                    </a>
-                ) : (<a />)}
-                
-                {this.props.settings !== undefined ? (
-                    <a href={`#settings/${
-                        this.props.settings == "" ? "general" : this.props.settings
-                    }`}>
-                        <span
-                            className="icon-settings"
-                            title="Settings"
-                        />{showText ? " Settings" : ""}
-                    </a>
-                ) : (<a />)}
-                
-                {this.props.books ? (
-                    <a href="#books/list">
-                        <span
-                            className="icon-book-open"
-                            title="Book List"
-                        />{showText ? " Books" : ""}
-                    </a>
-                ) : (<a />)}
+                <div className="right">
+                    {this.props.library ? (
+                        <a href="#library/info">
+                            <span
+                                className="icon-book"
+                                title="Library"
+                            />{showText ? " Library" : ""}
+                        </a>
+                    ) : (<a />)}
+                    
+                    {this.props.settings !== undefined ? (
+                        <a href={`#settings/${
+                            this.props.settings == "" ? "general" : this.props.settings
+                        }`}>
+                            <span
+                                className="icon-settings"
+                                title="Settings"
+                            />{showText ? " Settings" : ""}
+                        </a>
+                    ) : (<a />)}
+                    
+                    {this.props.books ? (
+                        <a href="#books/list">
+                            <span
+                                className="icon-book-open"
+                                title="Book List"
+                            />{showText ? " Books" : ""}
+                        </a>
+                    ) : (<a />)}
+                </div>
             </nav>
         );
     }
