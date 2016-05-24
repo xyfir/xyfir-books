fb3baf3891588a4ab565d567a1380d64537285eb
Mr. Xyfir <mr@xyfir.com>
2016-05-24 16:04:37 -0700

    Clean up output. Change theme in onSave()

--- client/components/settings/General.jsx
+++ client/components/settings/General.jsx
@@ -44,7 +44,7 @@ export default class GeneralSettings extends React.Component {
         const config = Object.assign({}, this.props.data.config);
         const theme = this.refs.theme.value;
         
-        // ** Change CSS theme file
+        document.body.className = "theme-" + theme;
         
         this.props.dispatch(setTheme(theme));
         
@@ -73,31 +73,28 @@ export default class GeneralSettings extends React.Component {
                         <option value="dark">Dark</option>
                     </select>
                 
-                    <button className="btn-primrary" onClick={this.onSave}>
+                    <button className="btn-primary" onClick={this.onSave}>
                         Save Settings
                     </button>
                 </section>
                 
+                <hr />
+                
                 <section className="storage">
-                    <h2>Storage</h2>
-                    <p>
-                        While your Libyq library is stored in the cloud so you can access it from any device with an internet connection, we also store data on your device to both prevent unnecessary data usage and allow you to access most of your library while not connected to the internet.
-                    </p>
-                    
-                    <hr />
-                    
-                    <h3>Clear Local Storage</h3>
+                    <h2>Clear Local Storage</h2>
                     <p>
                         Choose data to remove from your local storage. This data will still be available in the cloud and will be redownloaded and saved locally when you access it.
                     </p>
                     
-                    <input type="checkbox" ref="clear-covers" />Book Covers
-                    <input type="checkbox" ref="clear-books" />Book Files
-                    <input type="checkbox" ref="clear-metadata" />Book Metadata
-                    
                     <button className="btn-secondary" onClick={this.onClear}>
                         Clear Data
                     </button>
+                    
+                    <div>
+                        <input type="checkbox" ref="clear-covers" />Book Covers
+                        <input type="checkbox" ref="clear-books" />Book Files
+                        <input type="checkbox" ref="clear-metadata" />Book Metadata
+                    </div>
                 </section>
             </div>
         );
