1b6a3b814fb737088206b7a04eae88bdde848504
Mr. Xyfir <mr@xyfir.com>
2016-05-24 10:00:35 -0700

    Add module for generating a random number

---
+++ client/lib/random/number.js
@@ -0,0 +1,8 @@
+export default function (min, max, int = true) {
+    
+    if (int)
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    else
+        return Math.random() * (max - min + 1) + min;
+    
+}
