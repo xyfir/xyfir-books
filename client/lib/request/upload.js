467976143437dabb444bcba5c8874b51ed6b8a49
Mr. Xyfir <mr@xyfir.com>
2016-05-25 12:46:38 -0700

    Fix errors

--- client/lib/request/upload.js
+++ client/lib/request/upload.js
@@ -3,15 +3,13 @@ export default function (url, method, fieldName, files, fn) {
     let fd = new FormData();
     
     files.forEach(f => {
-        formData.append(fieldName, f);
+        fd.append(fieldName, f);
     });
 
     let xhr = new XMLHttpRequest();
 
     xhr.open(method, url, true);
-    
-    request.onload = () => fn(JSON.parse(this.responseText));
-
-    xhr.send(formData);
+    xhr.onload = () => fn(JSON.parse(xhr.responseText));
+    xhr.send(fd);
     
 };
