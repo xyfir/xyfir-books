2b7c3142c8ac00bf8dca47ac429036e3257f5619
Mr. Xyfir <mr@xyfir.com>
2016-05-30 12:42:57 -0700

    Initial commit

---
+++ client/components/books/read/Search.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class SearchBook extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="search-book">
+                
+            </div>
+        );
+    }
+
+}
---
+++ client/components/books/read/ViewBookmarks.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class ViewBookmarks extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="view-bookmarks">
+                
+            </div>
+        );
+    }
+
+}
