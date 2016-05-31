e67967b6bb5b7ef173952153a4b67bbafa0908f5
Mr. Xyfir <mr@xyfir.com>
2016-05-31 16:20:28 -0700

    Change ViewNotes -> Notes

--- client/components/books/read/Notes.jsx
+++ client/components/books/read/Notes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default class ViewNotes extends React.Component {
+export default class Notes extends React.Component {
 
     constructor(props) {
         super(props);
@@ -8,7 +8,7 @@ export default class ViewNotes extends React.Component {
 
     render() {
         return (
-            <div className="view-notes">
+            <div className="notes">
                 
             </div>
         );
