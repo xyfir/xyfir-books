af8d6d0d39d639d6bf181b8e5f9e741447b5b711
Mr. Xyfir <mr@xyfir.com>
2016-07-13 15:21:40 -0700

    Add annotationTypes

---
+++ client/constants/annotation-types.js
@@ -0,0 +1,11 @@
+const annotationTypes = {
+    1: { icon: "icon-document", name: "Document" },
+    2: { icon: "icon-link", name: "Link" },
+    3: { icon: "icon-search", name: "Search Query" },
+    4: { icon: "icon-image", name: "Image" },
+    5: { icon: "icon-video", name: "Video" },
+    6: { icon: "icon-sound", name: "Audio" },
+    7: { icon: "icon-location", name: "Map Location" }
+};
+
+export default annotationTypes;
