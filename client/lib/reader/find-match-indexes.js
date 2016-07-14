627f6e3209292556a444c53b9463124c4b35dcc7
Mr. Xyfir <mr@xyfir.com>
2016-07-14 16:41:31 -0700

    Define match variable

--- client/lib/reader/find-match-indexes.js
+++ client/lib/reader/find-match-indexes.js
@@ -3,7 +3,7 @@
 export default function(needle, haystack) {
 
     const pattern = new RegExp(needle, "gm");
-    let indexes = [];
+    let indexes = [], match;
 
     while (match = pattern.exec(haystack)) {
         indexes.push([match.index, pattern.lastIndex]);
