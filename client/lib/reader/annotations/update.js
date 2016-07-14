b0da326a58a43a5d30b6d0b10a9dbf9a5faa4509
Mr. Xyfir <mr@xyfir.com>
2016-07-14 16:42:20 -0700

    Add module for updating a book's annotations

---
+++ client/lib/reader/annotations/update.js
@@ -0,0 +1,41 @@
+// Modules
+import request from "lib/request/";
+
+// Constants
+import { LIB_ANN } from "constants/config";
+
+export default function(sets, key, fn) {
+
+    if (!sets || !sets.length || !key) {
+        fn([]);
+        return;
+    }
+
+    if (!navigator.onLine) {
+        fn(sets);
+        return;
+    }
+
+    // annotations.libyq.com/api/annotations?key=KEY&sets=SET|VERSION,...
+    let url = LIB_ANN + "annotations?key=" + key + "&sets="
+        + sets.map(set => set.id + "|" + set.version).join(",");
+
+    request({url, success: (res) => {
+        if (res.error) {
+            swal("Error", "Could not update annotations", "error");
+            fn([]);
+        }
+        else {
+            // Check if new version has been received
+            sets.forEach((set, i) => {
+                if (res[set.id] && res[set.id].version != set.version) {
+                    // Update version / items
+                    Object.assign(sets[i], res[set.id]);
+                }
+            });
+
+            fn(sets);
+        }
+    }});
+
+}
