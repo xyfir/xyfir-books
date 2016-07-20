130d79c63e52f798ad3ac48205e82934e2371b66
Mr. Xyfir <mr@xyfir.com>
2016-07-20 12:49:30 -0700

    Add light and dark reader themes

---
+++ client/constants/reader-themes.js
@@ -0,0 +1,9 @@
+export const LIGHT = {
+    backgroundColor: "#FFFFFF", color: "#000000", highlightColor: "#B1FB17",
+    annotationColor: "#85C1E9"
+};
+
+export const DARK = {
+    backgroundColor: "#1E1E1E", color: "#CCCCCC", highlightColor: "#1D8348",
+    annotationColor: "#21618C"
+};
