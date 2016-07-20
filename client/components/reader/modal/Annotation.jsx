58b0f539e54bda2aaa626d588e45252739f0d29c
Mr. Xyfir <mr@xyfir.com>
2016-07-19 17:06:00 -0700

    Add better output for each specific annotation type

--- client/components/reader/modal/Annotation.jsx
+++ client/components/reader/modal/Annotation.jsx
@@ -26,7 +26,7 @@ export default class Annotation extends React.Component {
         
         this.onChangeView = this.onChangeView.bind(this);
     }
-    
+
     onChangeView(view) {
         this.setState({ view });
     }
@@ -38,39 +38,102 @@ export default class Annotation extends React.Component {
 
         return (
             <div className="annotation">
-                <nav>{
-                    this.state.annotations.map(an => {
-                        return (
-                            <a
-                                onClick={() => this.onChangeView(an.type)}
-                                className={
-                                    annotationTypes[an.type].icon
-                                    + (
-                                        this.state.view == an.type
-                                        ? " active" : ""
-                                    )
-                                }
-                            />
-                        );
-                    })
-                }</nav>
+                    <div className="head">
+                    {this.state.annotations.length > 1 ? (
+                        <nav>{this.state.annotations.map(an => {
+                            return (
+                                <a
+                                    onClick={() => this.onChangeView(an.type)}
+                                    className={
+                                        annotationTypes[an.type].icon
+                                        + (
+                                            this.state.view == an.type
+                                            ? " active" : ""
+                                        )
+                                    }
+                                />
+                            );
+                        })}</nav>
+                    ) : <div />}
+                    
+                    <span className="annotation-name">{annotation.name}</span>
+                </div>
                 
-                <span className="annotation-name">{annotation.name}</span>
-                
-                {this.state.view == 1 ? (
+                <div className="value">{this.state.view == 1 ? (
                     <div
-                        className="document"
+                        className="markdown document"
                         dangerouslySetInnerHTML={{
                             __html: marked(annotation.value, { sanitize: true })
                         }}
                     />
+                ) : this.state.view == 2 ? (
+                    <div className="link">
+                        <a href={annotation.value} target="_blank">
+                            Go to Link
+                        </a>
+                        <iframe src={annotation.value} />
+                    </div>
                 ) : this.state.view == 3 ? (
-                    <iframe src={
-                        "https://www.google.com/#q=" + annotation.value
-                    } />
+                    <iframe
+                        src={"//www.bing.com/search?q=" + annotation.value}
+                        className="search"
+                    />
+                ) : this.state.view == 4 ? (
+                    <div className="image">
+                        <a href={annotation.value} target="_blank">
+                            Go to Image Link
+                        </a>
+                        <img src={annotation.value} />
+                    </div>
+                ) : this.state.view == 5 ? (
+                    <div className="video">{
+                        annotation.value.indexOf("youtube.com/") > -1 ? (
+                            <iframe
+                                src={annotation.value}
+                                className="youtube"
+                            />
+                        ) : annotation.value.indexOf("vimeo.com/") > -1 ? (
+                            <iframe
+                                src={annotation.value}
+                                className="viemo"
+                            />
+                        ) : (
+                            <div>
+                                <a href={annotation.value} target="_blank">
+                                    Go to Video Link
+                                </a>
+                                <video src={annotation.value} controls />
+                            </div>
+                        )
+                    }</div>
+                ) : this.state.view == 6 ? (
+                    <div className="audio">
+                        <a href={annotation.value} target="_blank">
+                            Go to Audio Link
+                        </a>
+                        <audio src={annotation.value} controls />
+                    </div>
                 ) : (
-                    <iframe src={annotation.value} />
-                )}
+                    <div className="map">{
+                        annotation.value.indexOf("http") == 0 ? (
+                            <div>
+                                <a href={annotation.value} target="_blank">
+                                    Go to Map Link
+                                </a>
+                                <iframe src={annotation.value} />
+                            </div>
+                        ) : (
+                            <iframe
+                                src={
+                                    "https://www.google.com/maps/embed/v1/place?key="
+                                    + "AIzaSyAN0om9mFmy1QN6Wf54tXAowK4eT0ZUPrU"
+                                    + "&q=" + annotation.value
+                                }
+                                className="gmaps"
+                            />
+                        )
+                    }</div>
+                )}</div>
             </div>
         )
     }
