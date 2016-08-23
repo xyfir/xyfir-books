9a89d02b72931aa15872989b3486717f66259b4b
MrXyfir <mr@xyfir.com>
2016-08-23 09:16:31 -0700

    Modify to output librarySizeLimit

--- controllers/account/info.js
+++ controllers/account/info.js
@@ -1,17 +1,32 @@
+const db = require("lib/db");
+
 /*
     GET api/account
     RETURN
         {
-            subscription: number, library: string
+            subscription:? number, library: string,
+            librarySizeLimit?: number
         }
     DESCRIPTION
         Return account info
 */
 module.exports = function(req, res) {
     
-    res.json({
-        subscription: req.session.subscription,
-        library: req.session.library
-    });
+    let sql = "SELECT library_size_limit FROM users WHERE user_id = ?";
+
+    db(cn => cn.query(sql, [req.session.uid], (err, rows) => {
+        cn.release();
+
+        if (err || !rows.length) {
+            res.json({ library: "" });
+        }
+        else {
+            res.json({
+                librarySizeLimit: rows[0].library_size_limit,
+                subscription: req.session.subscription,
+                library: req.session.library
+            });
+        }
+    }));
     
 };
