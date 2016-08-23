5b19c8d42e6334e63133306b71933ca6ce226ffc
MrXyfir <mr@xyfir.com>
2016-08-23 12:35:19 -0700

    Add PurchaseSubscription component

--- client/components/account/purchase/Subscription.jsx
+++ client/components/account/purchase/Subscription.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 // Action creators
-import { purchaseSubscription } from "actions/creators/account";
+import {
+    purchaseSubscription, setLibrarySizeLimit
+} from "actions/creators/account";
 
 // Constants
 import { URL, STRIPE_KEY_PUB } from "constants/config";
@@ -12,12 +14,14 @@ import request from "lib/request/index";
 // Components
 import NavBar from "../misc/NavBar";
 
-export default class Purchase extends React.Component {
+export default class PurchaseSubscription extends React.Component {
 
     constructor(props) {
         super(props);
 
-        this.onPurchase = this.onPurchase.bind(this);
+        this.state = {
+            showIncreaseSizeLimit: false, addToSizeLimit: 0
+        };
     }
 
     onPurchase() {
@@ -31,6 +35,7 @@ export default class Purchase extends React.Component {
             }
             
             let data = {
+                addToSizeLimit: this.state.addToSizeLimit,
                 subscription: +this.refs.subscription.value,
                 stripeToken: res.id
             };
@@ -42,20 +47,21 @@ export default class Purchase extends React.Component {
             
             request({
                 url: URL + "api/account/subscription",
-                method: "PUT", data, success: (res) => {
+                method: "POST", data, success: (res) => {
                     if (res.err) {
                         swal("Error", res.message, "error");
                     }
                     else {
                         const days = [0, 30, 182, 365][data.subscription];
-                        let subscription = this.props.data.account.subscription;
                         
-                        if (Date.now() > subscription)
-                            subscription = Date.now() + (days * 86400 * 1000);
-                        else
-                            subscription += (days * 86400 * 1000);
+                        let subscription = Date.now() + (days * 86400 * 1000);
                         
+                        // Update state.account.subscription|librarySizeLimit
                         this.props.dispatch(purchaseSubscription(subscription));
+                        this.props.dispatch(setLibrarySizeLimit(
+                            this.props.data.account.librarySizeLimit
+                            + data.addToSizeLimit
+                        ));
                         
                         swal(
                             "Purchase Complete",
@@ -68,9 +74,9 @@ export default class Purchase extends React.Component {
         })};
         
         // Dynamically load Stripe.js
-        let element = document.createElement('script');
-        element.src = 'https://js.stripe.com/v2/';
-        element.type = 'text/javascript';
+        let element = document.createElement("script");
+        element.src = "https://js.stripe.com/v2/";
+        element.type = "text/javascript";
         element.onload = purchase;
         document.body.appendChild(element);
     }
@@ -86,19 +92,13 @@ export default class Purchase extends React.Component {
                     settings={""}
                     books={true}
                 />
-            
-                <p>
-                    If you already have a subscription, the length of the subscription you purchase will be added to your current subscription.
-                </p>
                 
-                <hr />
-                
-                <div className="form">
+                <form className="form" onSubmit={() => this.onPurchase()}>
                     <select ref="subscription" defaultValue="0">
                         <option value="0" disabled>Subscription Length</option>
-                        <option value="1">1 Month   - $4</option>
-                        <option value="2">6 Months  - $21</option>
-                        <option value="3">12 Months - $36</option>
+                        <option value="1">30 Days  - $4</option>
+                        <option value="2">182 Days - $21</option>
+                        <option value="3">365 Days - $36</option>
                     </select>
                 
                     <form ref="stripeForm" className="stripe-form">
@@ -115,9 +115,43 @@ export default class Purchase extends React.Component {
                             <input type="number" data-stripe="exp-year" placeholder="2020" />
                         </div>
                     </form>
-                    
-                    <button onClick={this.onPurchase} className="btn-primary">Purchase</button>
-                </div>
+
+                    {this.state.showIncreaseSizeLimit ? (
+                        <div className="increase-size-limit">
+                            <p>
+                                A subscription with Libyq gives you 15 gigabytes of storage space for your ebook library. For most people, 15 gigabytes is more than enough to store thousands of ebooks.
+                                <br />
+                                Some people however have very large libraries that are above and beyond the 15 GB limit. If you're one of those people, you can increase your storage limit at a cost of <strong>$0.15</strong> per additional gigabyte over 15 GB. This is a monthly cost so 5 GB extra with a 6 months subscription would cost you $4.50 <em>((5 * 0.15) * 6)</em>.
+                            </p>
+
+                            <label>Add GB to Size Limit</label>
+                            <input
+                                onChange={
+                                    (e) => this.setState({ addToSizeLimit: e.target.value })
+                                }
+                                value={this.state.addToSizeLimit}
+                                type="number"
+                                step="1"
+                                min="0"
+                            />
+
+                            <span>
+                                <strong>Added cost per month:</strong> ${
+                                    this.state.addToSizeLimit * 0.15
+                                }
+                            </span>
+                        </div>
+                    ) : (
+                        <a onClick={
+                            () => this.setState({ showIncreaseSizeLimit: true })
+                        }>Increase Library Size Limit</a>
+                    )}
+                </form>
+
+                <button
+                    onClick={() => this.onPurchase()}
+                    className="btn-primary"
+                >Purchase</button>
             </div>
         )
     }
