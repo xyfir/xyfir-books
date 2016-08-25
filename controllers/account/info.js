dd2596a0e389fb6b2b74b376bc4d00ad26ee07ca
MrXyfir <mr@xyfir.com>
2016-08-25 11:36:04 -0700

    Grab all info from database instead of session

--- controllers/account/info.js
+++ controllers/account/info.js
@@ -12,21 +12,19 @@ const db = require("lib/db");
 */
 module.exports = function(req, res) {
     
-    let sql = "SELECT library_size_limit FROM users WHERE user_id = ?";
+    let sql = `
+        SELECT
+            library_size_limit as librarySizeLimit, subscription, library_id as library
+        FROM users WHERE user_id = ?
+    `;
 
     db(cn => cn.query(sql, [req.session.uid], (err, rows) => {
         cn.release();
 
-        if (err || !rows.length) {
+        if (err || !rows.length)
             res.json({ library: "" });
-        }
-        else {
-            res.json({
-                librarySizeLimit: rows[0].library_size_limit,
-                subscription: req.session.subscription,
-                library: req.session.library
-            });
-        }
+        else
+            res.json(rows[0]);
     }));
     
 };
