0a59f7c77b8140b47a2c2186d9a90382867d0c67
MrXyfir <mr@xyfir.com>
2016-09-23 09:58:04 -0700

    Add 'cycle highlight mode' control

--- client/components/reader/Overlay.jsx
+++ client/components/reader/Overlay.jsx
@@ -4,6 +4,32 @@ export default class ReaderOverlay extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = { status: "" };
+    }
+
+    onCycleHighlightMode() {
+        clearTimeout(this.timeout);
+
+        const hl = this.props.onCycleHighlightMode();
+        let status = "";
+
+        switch (hl.mode) {
+            case "none":
+                status = "Highlights turned off"; break;
+            
+            case "notes":
+                status = "Now highlighting notes"; break;
+
+            case "annotations":
+                status = "Now highlighting annotations from set "
+                    + this.props.book.annotations[hl.set].set_title;
+                break;
+        }
+
+        // Notify user of new highlight mode for 5 seconds
+        this.setState({ status });
+        this.timeout = setTimeout(() => this.setState({ status: "" }), 5000);
     }
 
     render() {
@@ -18,13 +44,16 @@ export default class ReaderOverlay extends React.Component {
                         className="next-page"
                         onClick={() => epub.nextPage()}
                     />
+                    <div
+                        className="cycle-highlight-mode"
+                        onClick={() => this.onCycleHighlightMode()}
+                    />
                 </div>
                 
-                <span
-                    onClick={() => this.props.onToggleAnnotations()}
-                    className="status"
-                >{
-                    this.props.loading ? (
+                <span className="status">{
+                    this.state.status ? (
+                        this.state.status.status
+                    ) : this.props.loading ? (
                         "Loading..."
                     ) : (
                         this.props.percent + "% | " + (
