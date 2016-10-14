c8b2afd3ceaf6b7e202561418f9425f1f6941125
MrXyfir <mr@xyfir.com>
2016-10-14 15:21:48 -0700

    Add DynamicStyles component

---
+++ client/components/misc/DynamicStyles.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+export default class DynamicStyles extends React.Component {
+	
+    constructor(props) {
+        super(props);
+
+        this.state = { styles: "" };
+    }
+
+    componentDidMount() {
+        if (this.props.beforeApp) {
+            this.setState({ styles: this._generateStylesBefore() });
+        }
+        else {
+            this.setState({ styles: this._generateStylesAfter() });
+        }
+    }
+
+    _generateStylesBefore() {
+        return `
+            #content {
+                margin-top: ${
+                    document.querySelector("nav.nav-bar").scrollHeight
+                    + (this._isPhoneGap() && this._isIOS() ? 20 : 0)
+                }px;
+            }
+            nav.nav-bar {${
+                this._isPhoneGap() && this._isIOS()
+                ? "padding-top: 20px;" : ""
+            }}
+        `;
+    }
+
+    _generateStylesAfter() {
+        return ``;
+    }
+
+    _isPhoneGap() {
+        return localStorage.getItem("isPhoneGap") == "true";
+    }
+
+    _isIOS() {
+        return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    }
+
+	render() {
+		return <style>{this.state.styles}</style>;
+	}
+
+}
