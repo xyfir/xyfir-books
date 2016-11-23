6028649f0481a47548781488dae878d3f8912fa8
MrXyfir <mr@xyfir.com>
2016-11-23 14:50:55 -0800

    Support new search system. Clean up code

--- client/components/books/list/all/Table.jsx
+++ client/components/books/list/all/Table.jsx
@@ -119,60 +119,79 @@ export default class TableList extends React.Component {
                 <div className="table-container"><table className="books">
                     <thead>
                         <tr>{
-                            this.props.data.config.bookList.table.columns.map(col => {
+                            this.props.data.config.bookList.table.columns
+                            .map(col => {
                                 return (
                                     <th
-                                        onContextMenu={() => window.location.hash = "settings/book-list"}
-                                        className={this.state.sort.column == col ? "sort-by" : ""}
+                                        onContextMenu={() => {
+                                            window.location.hash = "settings/book-list";
+                                        }}
+                                        className={
+                                            this.state.sort.column == col
+                                            ? "sort-by" : ""
+                                        }
                                         onClick={() => this.onSort(col)}
-                                    >{col.replace(/\b[a-z]/g, c => c.toUpperCase())}</th>
+                                    >{
+                                        col.replace(/\b[a-z]/g, c => c.toUpperCase())
+                                    }</th>
                                 )
                             })
                         }</tr>
                     </thead>
                     <tbody>{
                         sortBooks(
-                            findMatches(this.props.data.books, this.props.data.search),
-                            this.state.sort.column, this.state.sort.asc
+                            findMatches(
+                                this.props.data.books, this.props.data.search.query
+                            ), this.state.sort.column, this.state.sort.asc
                         ).map(book => {
                             return (
                                 <tr
-                                    className={
-                                        `book ${this.state.selected.indexOf(book.id) > -1 ? "selected" : ""}`
-                                    }
+                                    className={`book ${
+                                        this.state.selected.indexOf(book.id) > -1
+                                        ? "selected" : ""
+                                    }`}
                                     onClick={(e) => this.onSelect(e, book.id)}
                                 >{
-                                    this.props.data.config.bookList.table.columns.map(col => {
+                                    this.props.data.config.bookList.table.columns
+                                    .map(col => {
                                         switch (col) {
                                             case "added": return (
                                                 <td className="added">{
-                                                    (new Date(book.timestamp)).toLocaleDateString()
+                                                    (new Date(book.timestamp))
+                                                        .toLocaleDateString()
                                                 }</td>
                                             );
                                             
                                             case "rating": return (
                                                 <td className="rating">{
                                                     book.rating === undefined
-                                                        ? "No Rating"
-                                                        : <span>{book.rating}<span className="icon-star" /></span>
+                                                    ? "None" : (
+                                                        <span>
+                                                            {book.rating}
+                                                            <span className="icon-star" />
+                                                        </span>
+                                                    )
                                                 }</td>
                                             );
                                             
                                             case "published": return (
                                                 <td className="published">{
-                                                    (new Date(book.pubdate)).toLocaleDateString()
+                                                    (new Date(book.pubdate))
+                                                        .toLocaleDateString()
                                                 }</td>
                                             );
                                             
                                             case "series": return (
                                                 <td className="series">{
                                                     book.series === undefined
-                                                        ? "" : `${book.series} [${book.series_index}]`
+                                                    ? "" : `${book.series} [${book.series_index}]`
                                                 }</td>
                                             );
                                             
                                             default: return (
-                                                <td className={col}>{book[col]}</td>
+                                                <td className={col}>{
+                                                    book[col]
+                                                }</td>
                                             );
                                         }
                                     })
@@ -196,7 +215,9 @@ export default class TableList extends React.Component {
                             </a>
                             
                             {this.state.selected.length > 1 ? (
-                                <a href={`#books/bulk-edit/${this.state.selected.join(',')}`}>
+                                <a href={`#books/bulk-edit/${
+                                    this.state.selected.join(',')
+                                }`}>
                                     <span className="icon-edit" />Bulk Edit
                                 </a>
                             ) : (
@@ -215,10 +236,16 @@ export default class TableList extends React.Component {
                     
                     <div className="info">
                         <a href={`#books/read/${selectedBook.url}`}>
-                            <img className="cover" id={`cover-${selectedBook.id}`} />
+                            <img
+                                className="cover"
+                                id={`cover-${selectedBook.id}`}
+                            />
                         </a>
                         
-                        <span className="percent-complete">{selectedBook.percent_complete + "%"}</span>
+                        <span className="percent-complete">{
+                            selectedBook.percent_complete + "%"
+                        }</span>
+                        
                         {selectedBook.word_count > 0 ? (
                             <span className="word-count">{
                                 Math.round(selectedBook.word_count / 1000) + "K"
@@ -228,7 +255,8 @@ export default class TableList extends React.Component {
                         )}
                         
                         <span className="date-added">{
-                            (new Date(selectedBook.timestamp)).toLocaleDateString()
+                            (new Date(selectedBook.timestamp))
+                                .toLocaleDateString()
                         }</span>
                         {!!(+selectedBook.rating) ? (
                             <span className="rating">
@@ -239,29 +267,32 @@ export default class TableList extends React.Component {
                             <span />
                         )}
                         
-                        <hr />
-                        
                         <dl>
                             <dt>Title</dt><dd>{selectedBook.title}</dd>
                             
                             <dt>Authors</dt><dd><a href={
-                                `#books/list/all?authors=${encodeURIComponent(selectedBook.authors)}`
+                                `#books/list/all?search=1&authors=${
+                                    encodeURIComponent(selectedBook.authors)
+                                }`
                             }>{selectedBook.authors}</a></dd>
                             
                             {selectedBook.series !== undefined ? (
                                 <div>
                                     <dt>Series</dt>
                                     <dd>#{selectedBook.series_index} of <a href={
-                                        `#books/list/all?series=${encodeURIComponent(selectedBook.series)}`
+                                        `#books/list/all?search=1&series=${
+                                            encodeURIComponent(selectedBook.series)
+                                        }`
                                     }>{selectedBook.series}</a></dd>
                                 </div>
                             ) : (<div />)}
                             
                             <dt>Published</dt>
                             <dd>{
-                                (new Date(selectedBook.pubdate)).toLocaleDateString()
+                                (new Date(selectedBook.pubdate))
+                                    .toLocaleDateString()
                             } by <a href={
-                                "#books/list/all?publisher="
+                                "#books/list/all?search=1&publisher="
                                 + encodeURIComponent(selectedBook.publisher)
                             }>{
                                 selectedBook.publisher
@@ -271,11 +302,13 @@ export default class TableList extends React.Component {
                             <dd className="formats">{
                                 selectedBook.formats.map(format => {
                                     const url = LIBRARY_URL
-                                        + "files/" + this.props.data.account.library
+                                        + "files/"
+                                        + this.props.data.account.library
                                         + "/" + format;
                                     return ( 
                                         <a target="_blank" href={url}>{
-                                            format.split('.').slice(-1).toUpperCase()
+                                            format.split('.').slice(-1)[0]
+                                                .toUpperCase()
                                         }</a>
                                     );
                                 })
@@ -291,18 +324,14 @@ export default class TableList extends React.Component {
                                             <a
                                                 target="_blank"
                                                 href={`http://www.abebooks.com/book-search/isbn/${id[1]}`}
-                                            >
-                                                ISBN ({id[1]})
-                                            </a>
+                                            >ISBN ({id[1]})</a>
                                         );
                                         
                                         case "goodreads": return (
                                             <a
                                                 target="_blank"
                                                 href={`http://www.goodreads.com/book/show/${id[1]}`}
-                                            >
-                                                GoodReads
-                                            </a>
+                                            >GoodReads</a>
                                         );
                                         
                                         case "mobi-asin":
@@ -316,18 +345,17 @@ export default class TableList extends React.Component {
                                             <a
                                                 target="_blank"
                                                 href={`https://books.google.com/books/about/?id=${id[1]}`}
-                                            >
-                                                Google Books
-                                            </a>
+                                            >Google Books</a>
                                         );
                                         
                                         case "barnesnoble": return (
-                                            <a target="_blank" href={`http://www.barnesandnoble.com/${id[1]}`}>
-                                                Barnes & Noble
-                                            </a>
+                                            <a
+                                                target="_blank"
+                                                href={`http://www.barnesandnoble.com/${id[1]}`
+                                            }>Barnes & Noble</a>
                                         );
                                         
-                                        default: return (<span />);
+                                        default: return <span />;
                                     }
                                 })
                             }</dd>
@@ -336,19 +364,19 @@ export default class TableList extends React.Component {
                             <dd className="tags">{
                                 selectedBook.tags.map(tag => {
                                     return (
-                                        <a href={`#books/list/all?tag=${encodeURIComponent(tag)}`}>{
-                                            tag
-                                        }</a>
+                                        <a href={`#books/list/all?search=1&tag=${
+                                            encodeURIComponent(tag)}`
+                                        }>{tag}</a>
                                     );
                                 })
                             }</dd>
                         </dl>
                         
-                        <hr />
-                        
                         <div
                             className="comments"
-                            dangerouslySetInnerHTML={{__html: selectedBook.comments}}
+                            dangerouslySetInnerHTML={{
+                                __html: selectedBook.comments
+                            }}
                         />
                     </div>
                 </div>
