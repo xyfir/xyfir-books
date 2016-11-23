853a7a5f0c25a2d736a421268fca1fe698e49941
MrXyfir <mr@xyfir.com>
2016-11-23 14:48:19 -0800

    Support new search system. Put search info into alert. Clean up code

--- client/components/books/list/All.jsx
+++ client/components/books/list/All.jsx
@@ -5,29 +5,56 @@ import Compact from "./all/Compact";
 import Table from "./all/Table";
 import Grid from "./all/Grid";
 //
-import Search from "../../misc/Search";
+import Search from "components/misc/Search";
 
 // Modules
-import parseHashQuery from "../../../lib/url/parse-hash-query";
+import parseHashQuery from "lib/url/parse-hash-query";
 
 export default class ListAllBooks extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this._updateSearch = this._updateSearch.bind(this);
     }
     
     componentDidMount() {
+        this._updateSearch();
+    }
+
+    componentDidUpdate() {
+        this._updateSearch();
+    }
+
+    onSearchInfo() {
+        swal({
+            title: "Advanced Search Information",
+            text: `
+                For advanced searches you can search by metadata field
+                <br/><br />
+                <strong>Example:</strong> <code>authors:search title:contains_query</code>
+                <br /><br />
+                Use underscores in place of spaces when a search query for a field contains spaces.
+                <br />
+                Using multiple fields will return all books that match any of fields.
+            `,
+            html: true
+        });
+    }
+
+    _updateSearch() {
         const qo = parseHashQuery();
-        const qa = Object.keys(qo);
         
-        if (qa.length > 0) {
-            let value = "";
+        if (qo.search) {
+            delete qo.search;
+            const qa = Object.keys(qo);
+
+            const value = qa[0] + ":" + qo[qa[0]]
+                .replace(new RegExp(' ', 'g'), '_');
             
-            this.refs.search.setValue(
-                qa[0] + ":" + qo[qa[0]].replace(
-                    new RegExp(' ', 'g'), '_'
-                )
-            );
+            // Only set if value is different
+            if (this.refs.search.refs.search.value != value)
+                this.refs.search.setValue(value);
         }
     }
 
@@ -36,21 +63,22 @@ export default class ListAllBooks extends React.Component {
         
         switch (this.props.data.config.bookList.view) {
             case "compact":
-                view = <Compact data={this.props.data} dispatch={this.props.dispatch} />; break;
+                view = <Compact {...this.props} />; break;
             case "table":
-                view = <Table data={this.props.data} dispatch={this.props.dispatch} />; break;
+                view = <Table {...this.props} />; break;
             case "grid":
-                view = <Grid data={this.props.data} dispatch={this.props.dispatch} />;
+                view = <Grid {...this.props} />;
         }
         
         return (
             <div className="list-all">
-                <Search ref="search" dispatch={this.props.dispatch} />
-                <p>
-                    For advanced searches use <em>authors:search title:contains_query ...</em>.
-                    <br />
-                    Use underscores in place of spaces when a search query for a field contains spaces.
-                </p>
+                <div className="book-search">
+                    <Search ref="search" dispatch={this.props.dispatch} />
+                    <a
+                        onClick={() => this.onSearchInfo()}
+                        className="icon-info"
+                    />
+                </div>
                 
                 {view}
             </div>
