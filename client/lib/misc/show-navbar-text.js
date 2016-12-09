a4bebdbfde45b8462c4c58bbcf04b9448c5af949
MrXyfir <mr@xyfir.com>
2016-12-09 12:14:48 -0800

    Add lib/misc/show-navbar-text

---
+++ client/lib/misc/show-navbar-text.js
@@ -0,0 +1,25 @@
+function isNavbarOverflowed() {
+    if (sessionStorage.navbarOverflow == 1) return true;
+
+    const el = document.querySelector("nav.nav-bar");
+
+    if (el === null) return false;
+    
+    const isOverflowed =
+        el.clientWidth < el.scrollWidth
+        || el.clientHeight < el.scrollHeight;
+    
+    if (isOverflowed && !sessionStorage.navbarOverflow)
+        sessionStorage.navbarOverflow = 1;
+    
+    return isOverflowed;
+}
+
+export default function() {
+
+    if (window.innerHeight > window.innerWidth)
+        return false;
+    else
+        return !isNavbarOverflowed();
+
+}
