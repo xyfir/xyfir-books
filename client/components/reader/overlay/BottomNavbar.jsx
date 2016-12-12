9688e1bcefb8da0ca588972224e0bdbded03a1f9
MrXyfir <mr@xyfir.com>
2016-12-11 17:46:56 -0800

    Add progress slider

--- client/components/reader/overlay/BottomNavbar.jsx
+++ client/components/reader/overlay/BottomNavbar.jsx
@@ -1,3 +1,4 @@
+import Slider from "rc-slider";
 import React from "react";
 
 // Modules
@@ -28,6 +29,14 @@ export default class BottomReaderNavbar extends React.Component {
         }
     }
 
+    onGoToPercent(p) {
+        clearTimeout(this.timeout);
+
+        this.timeout = setTimeout(() => {
+            epub.gotoPercentage(p / 100);
+        }, 100);
+    }
+
     render() {
         const showText = showNavbarText(), r = this.props.reader;
         
@@ -35,26 +44,37 @@ export default class BottomReaderNavbar extends React.Component {
 
         return (
             <nav className="navbar bottom">
-                <a
-                    title="Go back"
-                    onClick={() => this.onGoBackInHistory()}
-                    disabled={
-                        !r.state.history.items.length || !r.state.history.index
-                    }
-                    className="icon-history"
-                >{showText ? "Back" : ""}</a>
+                <div className="progress-slider">
+                    <Slider
+                        tipTransitionName="rc-slider-tooltip-zoom-down"
+                        defaultValue={r.state.percent}
+                        onChange={p => this.onGoToPercent(p)}
+                    />
+                </div>
 
-                <a
-                    title="Book styling"
-                    onClick={() => r.onToggleShow("bookStyling")}
-                    className="icon-fontsize"
-                >{showText ? "Styling" : ""}</a>
+                <div className="items">
+                    <a
+                        title="Go back"
+                        onClick={() => this.onGoBackInHistory()}
+                        disabled={
+                            !r.state.history.items.length
+                            || !r.state.history.index
+                        }
+                        className="icon-history"
+                    >{showText ? "Back" : ""}</a>
 
-                <a
-                    title="Filters"
-                    onClick={() => r.onToggleShow("filters")}
-                    className="icon-light-up"
-                >{showText ? "Filters" : ""}</a>
+                    <a
+                        title="Book styling"
+                        onClick={() => r.onToggleShow("bookStyling")}
+                        className="icon-fontsize"
+                    >{showText ? "Styling" : ""}</a>
+
+                    <a
+                        title="Filters"
+                        onClick={() => r.onToggleShow("filters")}
+                        className="icon-light-up"
+                    >{showText ? "Filters" : ""}</a>
+                </div>
             </nav>
         );
     }
