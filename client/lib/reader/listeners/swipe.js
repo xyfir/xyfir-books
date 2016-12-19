f299055e64965e3fa041b4e7f7cf134da9c623c9
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-12-18 17:18:04 -0800

    Allow text selection and link clicking. Improve ease of swiping

--- client/lib/reader/listeners/swipe.js
+++ client/lib/reader/listeners/swipe.js
@@ -1,9 +1,9 @@
 export default function(el, fn) {
     
     // Required min distance traveled to be considered swipe
-    const threshold   = 150;
+    const threshold   = 100;
     // Maximum time allowed to travel that distance
-    const allowedTime = 300;
+    const allowedTime = 350;
     // Maximum distance allowed at the same time in perpendicular direction
     const restraint   = 100;
   
@@ -13,13 +13,6 @@ export default function(el, fn) {
         startX = e.changedTouches[0].pageX;
         startY = e.changedTouches[0].pageY;
         startTime = Date.now();
-        
-        e.preventDefault();
-    }, false);
-  
-    el.addEventListener("touchmove", e => {
-        // Prevent scrolling
-        e.preventDefault();
     }, false);
   
     el.addEventListener("touchend", e => {
@@ -29,34 +22,17 @@ export default function(el, fn) {
         
         // Time elapsed since touchstart
         const elapsedTime = Date.now() - startTime;
-
-        let direction = "";
         
         if (elapsedTime <= allowedTime) {
             // Horizontal swipe
             if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint)
                 // If dist traveled is negative, it indicates left swipe
-                direction = (distX < 0) ? "left" : "right";
+                fn((distX < 0) ? "left" : "right");
             // Vertical swipe
             else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint)
                 // If dist traveled is negative, it indicates up swipe
-                direction = (distY < 0) ? "up" : "down";
+                fn((distY < 0) ? "up" : "down");
         }
-
-        // User swiped in a direction
-        if (direction) {
-            fn(direction);
-        }
-        // Convert tap to a click
-        else {
-            epub.renderer.doc.documentElement.dispatchEvent(
-                new MouseEvent("click", {
-                    clientX: startX, clientY: startY
-                })
-            );
-        }
-        
-        e.preventDefault();
     }, false);
 
 }
