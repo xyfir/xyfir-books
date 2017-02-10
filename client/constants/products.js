a8baaaad27a49d3172597881977632bf3249a104
MrXyfir <mr@xyfir.com>
2017-02-10 13:21:24 -0800

    Modify to object that contains android and ios arrays

--- client/constants/products.js
+++ client/constants/products.js
@@ -1,5 +1,15 @@
-export default [
-    "com.xyfir.books.premium.30days",
-    "com.xyfir.books.premium.182days",
-    "com.xyfir.books.premium.365days"
-];
+export default {
+  
+  android: [
+    'com.xyfir.books.premium.30days',
+    'com.xyfir.books.premium.182days',
+    'com.xyfir.books.premium.365days'
+  ],
+
+  ios: [
+    'com.xyfir.books.premium.30',
+    'com.xyfir.books.premium.182',
+    'com.xyfir.books.premium.365'
+  ]
+
+};
