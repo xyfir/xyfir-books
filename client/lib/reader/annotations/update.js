77b9bcb5af1acb43fd710b08acc1282264bebd15
MrXyfir <mr@xyfir.com>
2017-03-20 10:13:17 -0700

    Changes, improvements
    - Change code style
    - Add JSDOC
    - Reflect changes made to xyAnnotations API
    - Use promise instead of callback

--- client/lib/reader/annotations/update.js
+++ client/lib/reader/annotations/update.js
@@ -1,41 +1,47 @@
-// Modules
-import request from "lib/request/index";
+import request from 'superagent';
 
 // Constants
-import { XYFIR_ANNOTATIONS } from "constants/config";
+import { XYFIR_ANNOTATIONS } from 'constants/config';
 
-export default function(sets, key, fn) {
+/**
+ * Updates the locally saved annotation sets.
+ * @param {object[]} sets - An array of annotation sets
+ * @param {string} key - Xyfir Annotations subscription key
+ * @returns {Promise} A promise that always resolves to an array of updated
+ * annotation sets, or an empty array on error.
+ */
+export default function(sets, key) {
 
-    if (!sets || !sets.length || !key) {
-        fn([]);
-        return;
+  return new Promise(resolve => {
+    if (!sets || !sets.length || !key || !navigator.onLine) {
+      resolve([]);
+      return;
     }
 
-    if (!navigator.onLine) {
-        fn(sets);
-        return;
-    }
-
-    // annotations.xyfir.com/api/annotations?key=KEY&sets=SET|VERSION,...
-    let url = XYFIR_ANNOTATIONS + "annotations?key=" + key + "&sets="
-        + sets.map(set => set.id + "|" + set.version).join(",");
-
-    request({url, success: (res) => {
-        if (res.error) {
-            swal("Error", "Could not update annotations", "error");
-            fn([]);
+    request
+      .get(XYFIR_ANNOTATIONS + 'annotations')
+      .query({
+        subscriptionKey: key, sets: JSON.stringify(
+          sets.map(set => Object({ id: set.id, version: set.version }))
+        )
+      })
+      .end((err, res) => {
+        if (err || res.body.error) {
+          swal('Error', 'Could not update annotations', 'error');
+          resolve([]);
         }
         else {
-            // Check if new version has been received
-            sets.forEach((set, i) => {
-                if (res[set.id] && res[set.id].version != set.version) {
-                    // Update version / items
-                    Object.assign(sets[i], res[set.id]);
-                }
-            });
+          // Check if new version has been received
+          sets.forEach((set, i) => {
+            if (res.body[set.id] && res.body[set.id].version != set.version) {
+              // Update version / items
+              Object.assign(sets[i], res.body[set.id]);
+            }
+          });
 
-            fn(sets);
+          resolve(sets);
         }
-    }});
+      });
+  });
 
 }
