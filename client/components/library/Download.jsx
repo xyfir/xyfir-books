6160b03a1a238ecf7ffc0cf4ff1f084644847fdc
MrXyfir <mr@xyfir.com>
2017-04-25 20:26:17 -0700

    Improvements
    - Change code style
    - Use superagent
    - Use based-blob
    - Add JSDOC documentation

--- client/components/library/Download.jsx
+++ client/components/library/Download.jsx
@@ -1,145 +1,133 @@
-import React from "react";
-import { saveAs } from "file-saver";
-
-// Modules
-import download from "lib/request/download";
+import { saveAs } from 'file-saver';
+import request from 'superagent';
+import React from 'react';
+import b from 'based-blob';
 
 // Components
-import NavBar from "../misc/NavBar";
+import NavBar from '../misc/NavBar';
 
 // Constants
-import { LIBRARY } from "constants/config";
+import { LIBRARY } from 'constants/config';
 
 export default class DownloadLibrary extends React.Component {
 
-    constructor(props) {
-        super(props);
-        
-        this.state = { status: "" };
-        
-        this.onDownload = this.onDownload.bind(this);
+  constructor(props) {
+    super(props);
+    
+    this.state = { status: '' };
+  }
+  
+  onDownload() {
+    if (!navigator.onLine) {
+      swal('Error', 'This action requires internet connectivity', 'error');
+      return;
     }
     
-    onDownload() {
-        if (!navigator.onLine) {
-            swal("Error", "This action requires internet connectivity", "error");
-            return;
-        }
+    const url = `${LIBRARY}files/${this.props.data.account.library}/`;
+    let zip = new JSZip;
+    
+    const downloadBook = index => {
+      const book = this.props.data.books[index];
+      
+      // All books downloaded
+      if (book === undefined) {
+        this.setState({ status: 'Downloading library database' });
         
-        let zip = new JSZip;
-        
-        const url = LIBRARY + "files/" + this.props.data.account.library + "/";
-        
-        const downloadBook = (index) => {
-            const book = this.props.data.books[index];
+        // Download metadata.db
+        request
+          .get(url + 'metadata.db')
+          .responseType('arraybuffer')
+          .end((err, res) => {
+            zip.file('metadata.db', res.body, { binary: true });
+          
+            this.setState({ status: 'Saving zip file' });
             
-            // All books downloaded
-            if (book === undefined) {
-                this.setState({ status: "Downloading library database" });
-                
-                // Download metadata.db
-                download(url + "metadata.db", res => {
-                    zip.file("metadata.db", res, { binary: true });
-                    
-                    this.setState({ status: "Saving zip file" });
-                    
-                    let b = zip.generate({});
+            const blob = zip.generate({});
+            zip = null;
 
-                    saveAs(
-                        this._base64ToBlob(b, "application/zip"),
-                        "Library - " + Date.now() + ".zip"
-                    );
-                    zip = null, b = null;
-                        
-                    this.setState({ status: "" });
-                }, true);
-            }
-            // Download next book
-            else {
-                let file = book.cover.split('/');
-                
-                this.setState({
-                    status: `Downloading book (${
-                        index + 1}/${this.props.data.books.length
-                    })`
-                });
-                
-                // Download cover                    
-                download(url + file.join('/'), res => {
-                    zip.file(file.join('/'), res, {binary: true});
-                    
-                    // Download metadata.opf
-                    file[2] = "metadata.opf";
-                    
-                    download(url + file.join('/'), res => {
-                        zip.file(file.join('/'), res, {binary: true});
-                        
-                        // Download formats
-                        book.formats.forEach(format => {
-                            download(url + format, res => {
-                                zip.file(format, res, {binary: true});
-                                
-                                downloadBook(index + 1);
-                            }, true);
-                        });
-                    }, true);
-                }, true);
-            }
-        };
+            saveAs(
+              b.toBlob(blob, 'application/zip'),
+              `Library_${Date.now()}.zip`
+            );
+              
+            this.setState({ status: '' });
+          });
+      }
+      // Download next book
+      else {
+        let file = book.cover.split('/');
         
-        downloadBook(0);
-    }
+        this.setState({
+          status: `Downloading book (${
+            index + 1}/${this.props.data.books.length
+          })`
+        });
 
-    _base64ToBlob(base64Data, contentType) {
-        contentType = contentType || '';
-        var sliceSize = 1024;
-        var byteCharacters = atob(base64Data);
-        var bytesLength = byteCharacters.length;
-        var slicesCount = Math.ceil(bytesLength / sliceSize);
-        var byteArrays = new Array(slicesCount);
+        // Download cover
+        request
+          .get(url + file.join('/'))
+          .responseType('arraybuffer')
+          .then(res => {
+            zip.file(file.join('/'), res.body, { binary: true });
 
-        for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
-            var begin = sliceIndex * sliceSize;
-            var end = Math.min(begin + sliceSize, bytesLength);
+            // Download metadata.opf
+            file[2] = 'metadata.opf';
 
-            var bytes = new Array(end - begin);
-            for (var offset = begin, i = 0 ; offset < end; ++i, ++offset) {
-                bytes[i] = byteCharacters[offset].charCodeAt(0);
-            }
-            byteArrays[sliceIndex] = new Uint8Array(bytes);
-        }
-        return new Blob(byteArrays, { type: contentType });
-    }
+            return request
+              .get(url + file.join('/'))
+              .responseType('arraybuffer')
+          })
+          .then(res => {
+            zip.file(file.join('/'), res.body, { binary: true });
 
-    render() {
-        return (
-            <div className="library-download">
-                <NavBar
-                    home={true}
-                    account={true}
-                    title="Download Library"
-                    library={true}
-                    settings={""}
-                    books={true}
-                />
-                
-                <section className="info">
-                    <p>
-                        Download your entire library in a zip file. The downloaded library will be completely compatible with <a target="_blank" href="https://calibre-ebook.com/">Calibre</a>.
-                    </p>
-                </section>
+            // Download formats
+            book.formats.forEach(format =>
+              request
+                .get(url + format)
+                .responseType('arraybuffer')
+                .end((err, res) =>
+                  zip.file(format, res.body, { binary: true })
+                )
+            );
 
-                <section className="download">                    
-                    {this.state.status == "" ? (
-                        <button className="btn-primary" onClick={this.onDownload}>
-                            Download
-                        </button>
-                    ) : (
-                        <span className="status">{this.state.status}</span>
-                    )}
-                </section>
-            </div>
-        );
-    }
+            downloadBook(index + 1);
+          })
+          .catch(err => 1)
+      }
+    };
+    
+    downloadBook(0);
+  }
+
+  render() {
+    return (
+      <div className='library-download'>
+        <NavBar
+          home={true}
+          account={true}
+          title='Download Library'
+          library={true}
+          settings={''}
+          books={true}
+        />
+        
+        <section className='info'>
+          <p>
+            Download your entire library in a zip file. The downloaded library will be completely compatible with <a target='_blank' href='https://calibre-ebook.com/'>Calibre</a>.
+          </p>
+        </section>
+
+        <section className='download'>                    
+          {this.state.status == '' ? (
+            <button className='btn-primary' onClick={() => this.onDownload()}>
+              Download
+            </button>
+          ) : (
+            <span className='status'>{this.state.status}</span>
+          )}
+        </section>
+      </div>
+    );
+  }
 
 }
