a8c676baad3417b5480506d141c2176e22819a0b
MrXyfir <mr@xyfir.com>
2017-05-31 16:05:00 -0700

    Reflect updates made to other projects

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,21 @@
 const gutil = require('gulp-util');
-const gzip = require('gulp-gzip');
+const sass = require('gulp-sass');
 const gulp = require('gulp');
 
-const postcss = require('gulp-postcss');
-const precss = require('precss');
-const nano = require('cssnano');
-const ap = require('autoprefixer');
+const config = require('./config');
 
-const isDev = require('./config').environment.type == 'dev';
+const prod = config.environment.type == 'prod';
+process.env.NODE_ENV = prod ? 'production' : config.environment.type;
 
-/*
-  css
-  - imports css files
-  - scss -> css
-  - autoprefixer
-  - minifies / gzip
-*/
-gulp.task('css', function() {
-  return gulp.src('./client/styles/style.css')
-    .pipe(postcss([
-      precss({}),
-      ap({browsers: 'last 1 version, > 10%'}),
-      nano({ autoprefixer: false, zindex: false })
-    ]))
-    //.pipe(!isDev ? gzip() : gutil.noop())
-    .pipe(gulp.dest('./static/css'));
-});
+gulp.task('css', () =>
+  gulp.src('./client/styles/style.css')
+    .pipe(
+      sass({ outputStyle: 'compressed' }).on('error', sass.logError)
+    )
+    .pipe(gulp.dest('./static/css'))
+);
 
-/*
-  client
-  - convert es2015 -> es5
-  - converts JSX -> plain JS
-  - bundles React components
-  - minifies / gzip
-*/
-gulp.task('client', function() {
+gulp.task('client', () => {
   const browserify = require('browserify');
   const streamify = require('gulp-streamify');
   const babelify = require('babelify');
@@ -54,69 +35,49 @@ gulp.task('client', function() {
   
   return b.bundle()
     .pipe(source('App.js'))
-    .pipe(streamify(uglify({
-      mangle: false,
-      compress: { unused: false }
-    })))
-    .on('error', gutil.log)
-    //.pipe(!isDev ? gzip() : gutil.noop())
+    .pipe(
+      prod ? streamify(
+        uglify({
+          mangle: false,
+          compress: { unused: false }
+        })
+      )
+      .on('error', gutil.log) : gutil.noop()
+    )
     .pipe(gulp.dest('./static/js/'));
 });
 
-/*
-  favicons
-  - generate favicons from icon.png
-  - place in static/icons
-*/
-gulp.task('favicons', function() {
+gulp.task('copy-libs', () =>
+  gulp.src([
+    './node_modules/sweetalert/dist/sweetalert.min.js',
+    './node_modules/localforage/dist/localforage.min.js',
+    './node_modules/epubjs/build/epub.min.js',
+    './client/lib/import/jszip.js'
+  ])
+  .pipe(gulp.dest('./static/js'))
+);
+
+gulp.task('copy-css', () =>
+  gulp.src([
+    './node_modules/sweetalert/dist/sweetalert.css'
+  ])
+  .pipe(
+    sass({ outputStyle: 'compressed' }).on('error', sass.logError)
+  )
+  .pipe(gulp.dest('./static/css'))
+);
+
+gulp.task('fontello', () =>
+  gulp.src('fontello.json')
+    .pipe(require('gulp-fontello')())
+    .pipe(gulp.dest('./static/fontello'))
+);
+
+gulp.task('favicons', () => {
   const favicons = require('gulp-favicons');
 
   return gulp.src('icon.png')
     .pipe(favicons({}))
     .on('error', gutil.log)
     .pipe(gulp.dest('./static/icons/'));
-});
-
-/*
-  copy-libs
-  - get localforage / sweetalert / epub.js / jszip
-  - copy to ./static/js
-*/
-gulp.task('copy-libs', function() {
-  return gulp.src([
-    './node_modules/sweetalert/dist/sweetalert.min.js',
-    './node_modules/localforage/dist/localforage.min.js',
-    './node_modules/epubjs/build/epub.min.js',
-    './client/lib/import/jszip.js'
-  ])
-  .pipe(gulp.dest('./static/js'));
-});
-
-/*
-  copy-css
-  - get sweetalert.css
-  - postcss modifiers
-  - copy to ./static/css
-*/
-gulp.task('copy-css', function() {
-  return gulp.src([
-    './node_modules/sweetalert/dist/sweetalert.css'
-  ])
-  .pipe(postcss([
-    ap({browsers: 'last 1 version, > 10%'}),
-    nano({ autoprefixer: false, zindex: false })
-  ]))
-  //.pipe(!isDev ? gzip() : gutil.noop())
-  .pipe(gulp.dest('./static/css'));
-});
-
-/*
-  fontello
-  - get font and css files from fontello
-  - place in ./static/fontello
-*/
-gulp.task('fontello', function() {
-  return gulp.src('fontello.json')
-    .pipe(require('gulp-fontello')())
-    .pipe(gulp.dest('./static/fontello'));
 });
