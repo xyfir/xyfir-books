b15712b5a222934ff50982adcb733175a25e33f8
MrXyfir <mr@xyfir.com>
2017-06-01 14:39:23 -0700

    Add items. Change code style

--- client/constants/initial-state.js
+++ client/constants/initial-state.js
@@ -1,30 +1,33 @@
 // Constants
-import * as readerThemes from "constants/reader-themes";
-import { LIST_BOOKS } from "./views";
+import * as readerThemes from 'constants/reader-themes';
+import { LIST_BOOKS } from './views';
 
-const state = {
-    books: [], view: LIST_BOOKS, save: "", account: {
-        subscription: 0, library: "", librarySizeLimit: 15
-    }, config: {
-        general: {
-            theme: "light"
-        },
-        bookList: {
-            view: "compact", table: {
-                columns: [
-                    "title", "authors", "series", "added", "rating"
-                ], defaultSort: {
-                    column: "title", asc: true
-                }
-            }
-        },
-        reader: Object.assign({}, readerThemes.LIGHT, {
-            fontSize: 1.2, padding: 1, lineHeight: 1.4, annotationsKey: "",
-            defaultHighlightMode: "none"
-        })
-    }, search: {
-        query: "", page: 1
-    }
+export default {
+  drawer: false, toasts: [],
+  books: [], view: LIST_BOOKS, save: '',
+  account: {
+    subscription: 0, library: '', librarySizeLimit: 15
+  },
+  config: {
+    general: {
+      theme: 'light'
+    },
+    bookList: {
+      view: 'compact', table: {
+        columns: [
+          'title', 'authors', 'series', 'added', 'rating'
+        ],
+        defaultSort: {
+          column: 'title', asc: true
+        }
+      }
+    },
+    reader: Object.assign({}, readerThemes.LIGHT, {
+      fontSize: 1.2, padding: 1, lineHeight: 1.4, annotationsKey: '',
+      defaultHighlightMode: 'none'
+    })
+  },
+  search: {
+    query: '', page: 1
+  }
 };
-
-export default state;
