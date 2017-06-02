9a7e3e9aa40b5b5168f83cd7045839d01c57281a
MrXyfir <mr@xyfir.com>
2017-06-02 14:42:18 -0700

    Updates
    - Change code style
    - Use superagent
    - Use lib/mysql
    - Add error messages
    - Update xyAccounts API usage

--- controllers/account/info.js
+++ controllers/account/info.js
@@ -1,127 +1,110 @@
-const request = require("request");
-const crypto = require("lib/crypto");
-const db = require("lib/db");
+const request = require('superagent');
+const crypto = require('lib/crypto');
+const mysql = require('lib/mysql');
 
-const config = require("config");
+const config = require('config');
 
 /*
-    GET api/account
-    REQUIRED
-        token: string
-    RETURN
-        {
-            library: string, subscription:? number, uid?: number, xadid?: string,
-            librarySizeLimit?: number, referral?: {
-                referral?: number, affiliate?: string,
-                hasMadePurchase?: boolean
-            }
-        }
-    DESCRIPTION
-        Creates a new session using access token
-        Return account info
+  GET api/account
+  REQUIRED
+    token: string
+  RETURN
+    {
+      error: boolean, message?: string,
+      library: string, subscription:? number, uid?: number, xadid?: string,
+      librarySizeLimit?: number,
+      referral?: {
+        referral?: number, affiliate?: string,
+        hasMadePurchase?: boolean
+      }
+    }
+  DESCRIPTION
+    Creates a new session using access token
+    Return account info
 */
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
 
-    // Wipe session, return empty library ID
-    const error = () => {
-        req.session.xadid = req.session.library = "";
-        req.session.uid = req.session.subscription = 0;
+  const db = new mysql;
 
-        res.json({ library: "" });
-    };
+  try {
+    let sql, vars, rows, uid;
 
-    db(cn => {
-        let sql = "";
+    await db.getConnection();
 
-        const getInfo = (uid) => {
-            sql = `
-                SELECT
-                    library_size_limit as librarySizeLimit, subscription,
-                    library_id as library, xad_id as xadid, referral,
-                    user_id as uid
-                FROM users WHERE user_id = ?
-            `;
+    // Validate access token
+    if (req.query.token) {
+      // [user_id, access_token]
+      const token = crypto.decrypt(
+        req.query.token, config.keys.accessToken
+      ).split('-');
 
-            cn.query(sql, [uid], (err, rows) => {
-                cn.release();
+      // Invalid token
+      if (!token[0] || !token[1]) throw 'Invalid access token';
 
-                if (err || !rows.length) {
-                    error();
-                }
-                else {
-                    rows[0].referral = JSON.parse(rows[0].referral);
+      // Get user's Xyfir ID
+      sql = `
+        SELECT xyfir_id FROM users WHERE user_id = ?
+      `,
+      vars = [
+        token[0]
+      ],
+      rows = await db.query(sql, vars);
 
-                    // Set session, return account info
-                    req.session.uid = uid,
-                    req.session.xadid = rows[0].xadid,
-                    req.session.library = rows[0].library,
-                    req.session.subscription = rows[0].subscription;
-                    
-                    res.json(rows[0]);
-                }
-            });
-        };
+      if (!rows.length) throw 'User does not exist';
 
-        // Validate access token
-        if (req.query.token) {
-            // [user_id, access_token]
-            const token = crypto.decrypt(
-                req.query.token, config.keys.accessToken
-            ).split('-');
+      // Validate access token with Xyfir Accounts
+      const xyAccRes = await request
+        .get(config.addresses.xyAccounts + 'api/service/14/user')
+        .query({
+          key: config.keys.xyAccounts, xid: rows[0].xyfir_id, token: token[1]
+        });
 
-            // Invalid token
-            if (!token[0] || !token[1]) {
-                cn.release();
-                error();
-                return;
-            }
+      if (xyAccRes.body.error)
+        throw 'xyAccounts error: ' + xyAccRes.body.message;
+        
+      uid = token[0];
+    }
+    // Get info for dev user
+    else if (config.environment.type == 'dev') {
+      uid = 1;
+    }
+    // Force login
+    else {
+      throw 'Access token required';
+    }
 
-            // Get user's Xyfir ID
-            sql = `SELECT xyfir_id FROM users WHERE user_id = ?`;
+    sql = `
+      SELECT
+        library_size_limit AS librarySizeLimit, subscription,
+        library_id AS library, xad_id AS xadid, referral,
+        user_id AS uid
+      FROM users WHERE user_id = ?
+    `,
+    vars = [
+      uid
+    ],
+    rows = await db.query(sql, vars);
 
-            cn.query(sql, [token[0]], (err, rows) => {
-                // User doesn't exist
-                if (err || !rows.length) {
-                    cn.release();
-                    error();
-                }
-                // Validate access token with Xyfir Accounts
-                else {
-                    let url = config.addresses.xacc + "api/service/14/"
-                    + `${config.keys.xacc}/${rows[0].xyfir_id}/${token[1]}`;
+    db.release();
 
-                    request(url, (err, response, body) => {
-                        // Error in request
-                        if (err) {
-                            cn.release();
-                            error();
-                            return;
-                        }
+    if (!rows.length) throw 'User does not exist';
 
-                        body = JSON.parse(body);
+    rows[0].referral = JSON.parse(rows[0].referral);
 
-                        // Error from Xyfir Accounts
-                        if (body.error) {
-                            cn.release();
-                            error();
-                        }
-                        // Access token valid
-                        else {
-                            getInfo(token[0]);
-                        }
-                    });
-                }
-            });
-        }
-        // Get info for dev user
-        else if (config.environment.type == "dev") {
-            getInfo(1);
-        }
-        // Force login
-        else {
-            cn.release();
-            error();
-        }
-    });
+    // Set session, return account info
+    rows[0].error = false,
+    req.session.uid = uid,
+    req.session.xadid = rows[0].xadid,
+    req.session.library = rows[0].library,
+    req.session.subscription = rows[0].subscription;
     
+    res.json(rows[0]);
+  }
+  catch (err) {
+    db.release();
+    req.session.destroy(e => 
+      res.json({ error: true, message: err, library: '' })
+    );
+  }
+  
 };
