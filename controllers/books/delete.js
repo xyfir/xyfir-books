529ae4940667f224c3aaa7b952c7960fefe8cf66
MrXyfir <mr@xyfir.com>
2017-06-02 15:49:42 -0700

    Updates, fixes
    - Change code style
    - Use lib/mysql
    - Use superagent
    - Fix deleting books
    - Add error messages

--- controllers/books/delete.js
+++ controllers/books/delete.js
@@ -1,51 +1,56 @@
-const request = require("request");
-const db = require("lib/db");
+const request = require('superagent');
+const mysql = require('lib/mysql');
 
-const config = require("config");
+const config = require('config');
 
 /*
-    DELETE api/books
-    REQUIRED
-        ids: string
-    RETURN
-        { error: boolean }
-    DESCRIPTION
-        Requests books to be deleted via library manager
-        Deletes books from database if removed from library
+  DELETE api/books
+  REQUIRED
+    ids: string
+  RETURN
+    { error: boolean, message?: string }
+  DESCRIPTION
+    Requests books to be deleted via library manager
+    Deletes books from database if removed from library
 */
-module.exports = function(req, res) { 
+module.exports = async function(req, res) {
 
-    request.del({
-        url: config.addresses.library + req.session.library + "/books",
-        form: { books: req.body.ids }
-    }, (err, response, body) => {
-        if (err) {
-            res.json({ error: true });
-        }
-        else {
-            body = JSON.parse(body);
-            
-            if (body.error) {
-                res.json(body);
-            }
-            else {
-                // Delete books and their bookmarks and notes
-                let sql = `
-                    DELETE b, bm, n
-                    FROM books b
-                    JOIN bookmarks bm ON bm.user_id = b.user_id AND bm.book_id = b.book_id
-                    JOIN notes n ON n.user_id = b.user_id AND n.book_id = b.book_id
-                    WHERE b.user_id = ? AND b.book_id IN (?)
-                `, vars = [
-                    req.session.uid, req.body.ids.split(',')
-                ];
-                
-                db(cn => cn.query(sql, vars, (err, result) => {
-                    cn.release();
-                    res.json({ error: !!err });
-                }));
-            }
-        }
-    });
+  const db = new mysql;
+
+  try {
+    const xyLibRes = await request
+      .delete(config.addresses.library + req.session.library + '/books')
+      .send({ books: req.body.ids });
+
+    if (xyLibRes.body.error) throw xyLibRes.body.message || 'xyLibrary error';
+
+    const vars = [
+      req.session.uid, req.body.ids.split(',')
+    ];
+
+    let sql = `
+      DELETE FROM books WHERE user_id = ? AND book_id IN (?)
+    `;
     
+    await db.getConnection();
+    await db.query(sql, vars);
+
+    sql = `
+      DELETE FROM bookmarks WHERE user_id = ? AND book_id IN (?)
+    `;
+    await db.query(sql, vars);
+
+    sql = `
+      DELETE FROM notes WHERE user_id = ? AND book_id IN (?)
+    `;
+    await db.query(sql, vars);
+    db.release();
+
+    res.json({ error: false });
+  }
+  catch (err) {
+    db.release();
+    res.json({ error: true, message: err });
+  }
+  
 };
