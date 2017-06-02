164c4f887d89e29cc2532fe000a3664d236feb4b
MrXyfir <mr@xyfir.com>
2017-06-01 20:30:07 -0700

    Add controller for GET api/account/logout

---
+++ controllers/account/logout.js
@@ -0,0 +1,12 @@
+const config = require('config');
+
+/*
+  GET api/account/logout
+  DESCRIPTION
+    Destroy user's session
+*/
+module.exports = function(req, res) {
+
+  req.session.destroy(err => res.redirect(config.addresses.xyBooks));
+
+}
