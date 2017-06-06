039d150cb3255162c88fbb8a06a152e970a30736
MrXyfir <mr@xyfir.com>
2017-06-05 17:33:00 -0700

    Add 'email upload' controller. Update code style

--- controllers/index.js
+++ controllers/index.js
@@ -1,32 +1,62 @@
 const router = require('express').Router();
 
 /* Library Manager */
-
-router.post('/library-manager/:lib/library', require('./library-manager/add-library'));
-router.post('/library-manager/:lib/books', require('./library-manager/add-books'));
-router.put('/library-manager/:lib/books/:book', require('./library-manager/increment-version'));
+router.post(
+  '/library-manager/:lib/library',
+  require('./library-manager/add-library')
+);
+router.post(
+  '/library-manager/:lib/books',
+  require('./library-manager/add-books')
+);
+router.put(
+  '/library-manager/:lib/books/:book',
+  require('./library-manager/increment-version')
+);
 
 /* Books */
-
-router.route('/books')
-  .get(require('./books/list'))
-  .delete(require('./books/delete'));
-
-router.get('/books/:book', require('./books/open'));
-router.post('/books/:book/close', require('./books/close'));
-
-router.route('/books/:book/bookmark')
-  .post(require('./books/bookmark/add'))
-  .delete(require('./books/bookmark/remove'));
-
-router.route('/books/:book/note')
-  .post(require('./books/note/add'))
-  .delete(require('./books/note/remove'));
-  
-router.put('/books/:book/word-count', require('./books/word-count'));
+router.get(
+  '/books',
+  require('./books/list')
+);
+router.delete(
+  '/books',
+  require('./books/delete')
+);
+router.post(
+  '/books/email-upload',
+  require('./books/email-upload')
+);
+router.get(
+  '/books/:book',
+  require('./books/open')
+);
+router.post(
+  '/books/:book/close',
+  require('./books/close')
+);
+router.post(
+  '/books/:book/bookmark',
+  require('./books/bookmark/add')
+);
+router.delete(
+  '/books/:book/bookmark',
+  require('./books/bookmark/remove')
+);
+router.put(
+  '/books/:book/word-count',
+  require('./books/word-count')
+);
+router.post(
+  '/books/:book/note',
+  require('./books/note/add')
+);
+router.post(
+  '/books/:book/note',
+  require('./books/note/remove')
+);
 
 /* Account */
-
 router.get('/account', require('./account/info'));
 router.post('/account/login', require('./account/login'));
 router.get('/account/logout', require('./account/logout'));
