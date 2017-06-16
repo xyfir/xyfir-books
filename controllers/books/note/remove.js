a8093b314569fb57ee7523c8f958db2a5fe2955b
MrXyfir <mr@xyfir.com>
2017-06-15 20:22:47 -0700

    Improvements, changes
    - Use lib/mysql
    - Update code style
    - Use created in place of cfi

--- controllers/books/note/remove.js
+++ controllers/books/note/remove.js
@@ -1,22 +1,33 @@
-const db = require("../../../lib/db");
+const mysql = require('lib/mysql');
 
 /*
-    DELETE api/books/:book/note
-    REQUIRED
-        cfi: string
-    RETURN
-        { error: boolean }
-    DESCRIPTION
-        Delete note at body.cfi from :book
+  DELETE api/books/:book/note
+  REQUIRED
+    created: number
+  RETURN
+    { error: boolean }
 */
-module.exports = function(req, res) { 
+module.exports = async function(req, res) {
 
-    const sql = `DELETE FROM notes WHERE user_id = ? AND book_id = ? AND cfi = ?`;
-    const vars = [req.session.uid, req.params.book, req.body.cfi];
-    
-    db(cn => cn.query(sql, vars, (err, result) => {
-        cn.release();
-        res.json({ error: !!err || !result.affectedRows });
-    }));
-    
+  const db = new mysql;
+
+  try {
+    await db.getConnection();
+
+    const sql = `
+      DELETE FROM notes WHERE user_id = ? AND book_id = ? AND created = ?
+    `,
+    vars = [
+      req.session.uid, req.params.book, req.body.created
+    ],
+    result = await db.query(sql, vars);
+
+    db.release();
+    res.json({ error: !result.affectedRows });
+  }
+  catch (err) {
+    db.release();
+    res.json({ error: true });
+  }
+  
 };
