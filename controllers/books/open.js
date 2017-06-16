879b668a1406e27aec53b1b80d684bcf9aa24475
MrXyfir <mr@xyfir.com>
2017-06-15 20:20:59 -0700

    Improvements, changes
    - Use lib/mysql
    - Update code style
    - Change columns for notes

--- controllers/books/open.js
+++ controllers/books/open.js
@@ -1,54 +1,79 @@
-const db = require("../../lib/db");
+const mysql = require('lib/mysql');
 
 /*
-    GET api/books/:book
-    RETURN
-        {
-            notes: [{ cfi: string, note: string, created: number }],
-            bookmarks: [{ cfi: string, created: number }],
-            last_read: number
+  GET api/books/:book
+  RETURN
+    {
+      notes: [{
+        cfi: string, content: string, created: number, highlights: string[],
+        range: {
+          start: string, end: string
         }
-    DESCRIPTION
-        Returns all notes / highlighted text and bookmarks for a book
-        Update book's last_read
+      }],
+      bookmarks: [{
+        cfi: string, created: number
+      }],
+      last_read: number
+    }
+  DESCRIPTION
+    Returns all notes / highlighted text and bookmarks for a book
+    Update book's last_read
 */
-module.exports = function(req, res) { 
+module.exports = async function(req, res) {
+
+  const db = new mysql;
+
+  try {
+    await db.getConnection();
 
     // Get notes
-    let vars = [req.session.uid, req.params.book];
     let sql = `
-        SELECT cfi, note, created FROM notes WHERE user_id = ? AND book_id = ?
-        ORDER BY created DESC
-    `;
+      SELECT
+        cfi, created, content, highlights, cfi_range AS 'range'
+      FROM notes
+        WHERE user_id = ? AND book_id = ?
+      ORDER BY created DESC
+    `,
+    vars = [
+      req.session.uid, req.params.book
+    ],
+    rows = await db.query(sql, vars);
+
+    const response = { bookmarks: [], notes: rows, last_read: 0 };
+
+    // Get bookmarks
+    sql = `
+      SELECT cfi, created FROM bookmarks WHERE user_id = ? AND book_id = ?
+      ORDER BY created DESC
+    `,
+    rows = await db.query(sql, vars);
+
+    response.bookmarks = rows,
+    response.last_read = Date.now();
+
+    // Update last_read
+    sql = `
+      UPDATE books SET last_read = ? WHERE user_id = ? AND book_id = ?
+    `,
+    vars = [
+      response.last_read, req.session.uid, req.params.book
+    ];
     
-    db(cn => cn.query(sql, vars, (err, rows) => {
-        let response = { bookmarks: [], notes: [], last_read: 0 };
-        
-        if (err) {
-            cn.release();
-            res.json(response);
-        }
-        else {
-            response.notes = rows;
-            
-            // Get bookmarks
-            sql = `
-                SELECT cfi, created FROM bookmarks WHERE user_id = ? AND book_id = ?
-                ORDER BY created DESC
-            `;
-            cn.query(sql, vars, (err, rows) => {
-                response.bookmarks = rows;
-                response.last_read = Date.now();
-                
-                res.json(response);
-                
-                // Update last_read
-                sql = "UPDATE books SET last_read = ? WHERE user_id = ? AND book_id = ?";
-                vars = [response.last_read, req.session.uid, req.params.book];
-                
-                cn.query(sql, vars, (err, result) => cn.release());
-            });
-        }
-    }));
-    
+    await db.query(sql, vars);
+    db.release();
+
+    // Parse highlights and range
+    response.notes = response.notes.map(note => {
+      note.range = JSON.parse(note.range),
+      note.highlights = JSON.parse(note.highlights);
+      return note;
+    });
+
+    res.json(response);
+  }
+  catch (err) {
+    db.release();
+    res.json({ error: true, message: err });
+  }
+  
 };
