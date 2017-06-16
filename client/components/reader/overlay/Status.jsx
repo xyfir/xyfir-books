22bda4a454998b72fa5ec8ebe3b344a40dd9e419
MrXyfir <mr@xyfir.com>
2017-06-16 16:05:24 -0700

    Update code style

--- client/components/reader/overlay/Status.jsx
+++ client/components/reader/overlay/Status.jsx
@@ -1,52 +1,51 @@
-import React from "react";
+import React from 'react';
 
 export default class ReaderStatus extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = { status: "" };
+    this.state = { status: '' };
+  }
+
+  _setStatus(hl) {
+    clearTimeout(this.timeout);
+
+    const status = (() => {
+      switch (hl.mode) {
+        case 'none':
+          return 'Highlights turned off';
+        
+        case 'notes':
+          return 'Now highlighting notes';
+
+        case 'annotations':
+          return 'Now highlighting annotations from set ' +
+            this.props.book.annotations[hl.index].set_title;
+      }
+    })();
+
+    // Notify user of new highlight mode for 5 seconds
+    this.setState({ status });
+    this.timeout = setTimeout(() => this.setState({ status: '' }), 5000);
+  }
+
+  render() {
+    let status = '';
+
+    if (this.state.status)
+      status = this.state.status;
+    else if (this.props.loading)
+      status = 'Loading...';
+    else {
+      status = this.props.percent + '% | ' + (
+        !this.props.pagesLeft ?
+        'Last page in chapter' :
+        this.props.pagesLeft + ' pages left in chapter'
+      )
     }
-
-    _setStatus(hl) {
-        clearTimeout(this.timeout);
-
-        let status = "";
-
-        switch (hl.mode) {
-            case "none":
-                status = "Highlights turned off"; break;
-            
-            case "notes":
-                status = "Now highlighting notes"; break;
-
-            case "annotations":
-                status = "Now highlighting annotations from set "
-                    + this.props.book.annotations[hl.index].set_title;
-                break;
-        }
-
-        // Notify user of new highlight mode for 5 seconds
-        this.setState({ status });
-        this.timeout = setTimeout(() => this.setState({ status: "" }), 5000);
-    }
-
-    render() {
-        return (
-            <span className="status">{
-                this.state.status ? (
-                    this.state.status
-                ) : this.props.loading ? (
-                    "Loading..."
-                ) : (
-                    this.props.percent + "% | " + (
-                        !this.props.pagesLeft
-                        ? "Last page in chapter"
-                        : this.props.pagesLeft + " pages left in chapter"
-                    )
-                ) 
-            }</span>
-        );
-    }
+    
+    return <span className='status'>{status}</span>
+  }
 
 }
