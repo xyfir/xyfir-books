3605b71b2180415747460166dab819d7d3e81133
MrXyfir <mr@xyfir.com>
2017-06-15 20:22:24 -0700

    Improvements, changes
    - Use lib/mysql
    - Update code style
    - Change columns / data

--- controllers/books/note/add.js
+++ controllers/books/note/add.js
@@ -1,26 +1,38 @@
-const db = require("../../../lib/db");
+const mysql = require('lib/mysql');
 
 /*
-    POST api/books/:book/note
-    REQUIRED
-        cfi: string, note: string
-    RETURN
-        { error: boolean }
-    DESCRIPTION
-        Adds a note to highlighted text at body.cfi in :book
+  POST api/books/:book/note
+  REQUIRED
+    cfi: string, content: string, highlights: json-string, range: json-string,
+    created: number
+  RETURN
+    { error: boolean }
+  DESCRIPTION
+    Adds a note to highlighted text at body.cfi in :book
 */
-module.exports = function(req, res) { 
+module.exports = async function(req, res) {
 
-    const insert = {
-        user_id: req.session.uid, book_id: req.params.book,
-        cfi: req.body.cfi, note: req.body.note,
-        created: Date.now()
-    };
-    const sql = `INSERT INTO notes SET ?`;
-    
-    db(cn => cn.query(sql, insert, (err, result) => {
-        cn.release();
-        res.json({ error: !!err || !result.affectedRows });
-    }));
-    
+  const db = new mysql;
+
+  try {
+    await db.getConnection();
+
+    const sql = `
+      INSERT INTO notes SET ?
+    `,
+    insert = {
+      user_id: req.session.uid, book_id: req.params.book, cfi: req.body.cfi,
+      content: req.body.content, highlights: req.body.highlights,
+      cfi_range: req.body.range, created: req.body.created
+    },
+    result = await db.query(sql, insert);
+
+    db.release();
+    res.json({ error: !result.affectedRows });
+  }
+  catch (err) {
+    db.release();
+    res.json({ error: true });
+  }
+  
 };
