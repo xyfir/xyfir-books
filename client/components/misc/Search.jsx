fd69f0a2bdccd86254acf159b5595a7d22122e1e
MrXyfir <mr@xyfir.com>
2017-06-18 12:45:13 -0700

    Use react-md. Update code style

--- client/components/misc/Search.jsx
+++ client/components/misc/Search.jsx
@@ -1,52 +1,60 @@
-import React from "react";
+import React from 'react';
+
+// react-md
+import TextField from 'react-md/lib/TextFields';
+import Paper from 'react-md/lib/Papers';
 
 // Action creators
-import { setSearch } from "actions/creators/index";
+import { setSearch } from 'actions/creators/index';
 
 // Modules
-import parseQuery from "lib/url/parse-hash-query";
+import parseQuery from 'lib/url/parse-hash-query';
 
 export default class Search extends React.Component {
 
-    constructor(props) {
-        super(props);
-        
-        this.setValue = this.setValue.bind(this);
-    }
+  constructor(props) {
+    super(props);
     
-    componentWillUnmount() {
-        this.props.dispatch(setSearch(""));
-    }
-    
-    onSearch(sv) {
-        clearTimeout(this.timeout);
+    this.setValue = this.setValue.bind(this);
+  }
+  
+  componentWillUnmount() {
+    this.props.dispatch(setSearch(''));
+  }
+  
+  onSearch(sv) {
+    clearTimeout(this.timeout);
 
-        // Clear query string
-        if (!sv && parseQuery().search) {
-            location.hash = location.hash.split('?')[0];
-        }
-        
-        this.timeout = setTimeout(() => {
-            this.props.dispatch(setSearch(
-                this.refs.search.value.toLowerCase()
-            ))
-        }, 150);
+    // Clear query string
+    if (!sv && parseQuery().search) {
+      location.hash = location.hash.split('?')[0];
     }
     
-    setValue(val) {
-        this.refs.search.value = val;
-        this.onSearch(true);
-    }
+    this.timeout = setTimeout(() =>
+      this.props.dispatch(
+        setSearch(this.refs.search.getField().value.toLowerCase())
+      ), 150
+    );
+  }
+  
+  setValue(val) {
+    this.refs.search.value = val;
+    this.onSearch(true);
+  }
 
-    render() {
-        return (
-            <input
-                ref="search"
-                type="text"
-                onChange={() => this.onSearch()}
-                placeholder={this.props.placeholder || "Search"}
-            />
-        );
-    }
+  render() {
+    return (
+      <Paper zDepth={1} className='search section'>
+        <TextField
+          block paddedBlock
+          id='search'
+          ref='search'
+          type='search'
+          onChange={e => this.onSearch()}
+          placeholder='Search'
+        />
+      </Paper>
+    );
+  }
 
 }
