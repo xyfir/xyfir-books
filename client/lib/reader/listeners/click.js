31be1822dc5ebd50a365aef2086e8c1f808fe4c5
MrXyfir <mr@xyfir.com>
2017-06-18 11:25:33 -0700

    Update code style. Use JSDOC

--- client/lib/reader/listeners/click.js
+++ client/lib/reader/listeners/click.js
@@ -1,35 +1,38 @@
+/**
+ * Listen for clicks and convert them to actions based on the location of the 
+ * click on the page.
+ * @param {HTMLElement} el - The element to add event listeners to.
+ * @param {function} fn - The listener function.
+ */
 export default function(el, fn) {
 
-    el.addEventListener("click", event => {
-        // User selected text
-        if (!!epub.renderer.selectedRange.toString()) {
-            return;
-        }
-        else {
-            // Get book iframe window's size
-            const wX = epub.renderer.doc.defaultView.innerWidth
-                , wY = epub.renderer.doc.defaultView.innerHeight;
-            
-            // Get click location
-            const cX = event.clientX
-                , cY = event.clientY;
-            
-            // Click was in left 10% of page
-            if (cX < wX * 0.10)
-                fn("previous page");
-            // Click was in right 10% of page
-            else if (cX > wX - (wX * 0.10))
-                fn("next page");
-            // Click was in top 5% of page
-            else if (cY < wY * 0.05)
-                fn("show book info");
-            // Click was in bottom 5% of page
-            else if (cY > wY - (wY * 0.05))
-                fn("cycle highlights");
-            // Click was somewhere in middle
-            else
-                fn("toggle navbar");
-        }
-    }, false);
+  el.addEventListener('click', event => {
+    // User selected text
+    if (!!epub.renderer.selectedRange.toString()) return;
+    
+    // Get book iframe window's size
+    const wX = epub.renderer.doc.defaultView.innerWidth;
+    const wY = epub.renderer.doc.defaultView.innerHeight;
+    
+    // Get click location
+    const cX = event.clientX;
+    const cY = event.clientY;
+    
+    // Click was in left 10% of page
+    if (cX < wX * 0.10)
+      fn('previous page');
+    // Click was in right 10% of page
+    else if (cX > wX - (wX * 0.10))
+      fn('next page');
+    // Click was in top 5% of page
+    else if (cY < wY * 0.05)
+      fn('show book info');
+    // Click was in bottom 5% of page
+    else if (cY > wY - (wY * 0.05))
+      fn('cycle highlights');
+    // Click was somewhere in middle
+    else
+      fn('toggle navbar');
+  }, false);
 
 }
--- client/lib/reader/listeners/swipe.js
+++ client/lib/reader/listeners/swipe.js
@@ -1,50 +1,56 @@
+/**
+ * Listen for swipes convert them to actions.
+ * @param {HTMLElement} el - The element to add event listeners to.
+ * @param {function} fn - The listener function.
+ */
 export default function(el, fn) {
+  
+  // Required min distance traveled to be considered swipe
+  const threshold   = 100;
+  // Maximum time allowed to travel that distance
+  const allowedTime = 350;
+  // Maximum distance allowed at the same time in perpendicular direction
+  const restraint   = 100;
+  
+  let startX, startY, startTime;
+
+  el.addEventListener('touchstart', e => {
+    startX = e.changedTouches[0].pageX;
+    startY = e.changedTouches[0].pageY;
+    startTime = Date.now();
+  }, false);
+  
+  el.addEventListener('touchend', e => {
+    // Get distance traveled by finger while in contact with surface
+    const distX = e.changedTouches[0].pageX - startX;
+    const distY = e.changedTouches[0].pageY - startY;
     
-    // Required min distance traveled to be considered swipe
-    const threshold   = 100;
-    // Maximum time allowed to travel that distance
-    const allowedTime = 350;
-    // Maximum distance allowed at the same time in perpendicular direction
-    const restraint   = 100;
-  
-    let startX, startY, startTime;
+    // Time elapsed since touchstart
+    const elapsedTime = Date.now() - startTime;
+    
+    if (elapsedTime <= allowedTime) {
+      // Horizontal swipe
+      if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint)
+        // If dist traveled is negative, it indicates left swipe
+        fn((distX < 0) ? 'left' : 'right');
+      // Vertical swipe
+      else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint)
+        // If dist traveled is negative, it indicates up swipe
+        fn((distY < 0) ? 'up' : 'down');
+      // Tap
+      else {
+        epub.renderer.doc.defaultView.getSelection().removeAllRanges();
 
-    el.addEventListener("touchstart", e => {
-        startX = e.changedTouches[0].pageX;
-        startY = e.changedTouches[0].pageY;
-        startTime = Date.now();
-    }, false);
-  
-    el.addEventListener("touchend", e => {
-        // Get distance traveled by finger while in contact with surface
-        const distX = e.changedTouches[0].pageX - startX;
-        const distY = e.changedTouches[0].pageY - startY;
+        // Convert tap to click
+        epub.renderer.doc.documentElement.dispatchEvent(
+          new MouseEvent('click', {
+            clientX: startX, clientY: startY
+          })
+        );
         
-        // Time elapsed since touchstart
-        const elapsedTime = Date.now() - startTime;
-        
-        if (elapsedTime <= allowedTime) {
-            // Horizontal swipe
-            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint)
-                // If dist traveled is negative, it indicates left swipe
-                fn((distX < 0) ? "left" : "right");
-            // Vertical swipe
-            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint)
-                // If dist traveled is negative, it indicates up swipe
-                fn((distY < 0) ? "up" : "down");
-            // Tap
-            else {
-                epub.renderer.doc.defaultView.getSelection().removeAllRanges();
-
-                epub.renderer.doc.documentElement.dispatchEvent(
-                    new MouseEvent("click", {
-                        clientX: startX, clientY: startY
-                    })
-                );
-                
-                e.preventDefault();
-            }
-        }
-    }, false);
+        e.preventDefault();
+      }
+    }
+  }, false);
 
 }
