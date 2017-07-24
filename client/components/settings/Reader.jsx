46a843f958167cdb5baa8e41d42ad9b0d7d443f0
MrXyfir <mr@xyfir.com>
2017-07-24 14:35:52 -0700

    Improvements, changes
    - Use react-md / remove .old
    - Prevent saving theme / custom styling if user does not have a subscription
    - Use <ColorPicker>

--- client/components/settings/Reader.jsx
+++ client/components/settings/Reader.jsx
@@ -8,20 +8,24 @@ import initialState from 'constants/initial-state';
 import { setReader } from 'actions/creators/settings';
 import { save } from 'actions/creators/index';
 
+// react-md
+import SelectField from 'react-md/lib/SelectFields';
+import TextField from 'react-md/lib/TextFields';
+import Button from 'react-md/lib/Buttons/Button';
+import Paper from 'react-md/lib/Papers';
+
+// Components
+import ColorPicker from 'components/misc/ColorPicker';
+
 export default class ReaderSettings extends React.Component {
 
   constructor(props) {
     super(props);
     
     this.state = this.props.data.config.reader;
-    
-    this.onReset = this.onReset.bind(this);
-    this.onSave = this.onSave.bind(this);
   }
 
-  onSetTheme() {
-    const theme = this.refs.theme.value;
-
+  onSetTheme(theme) {
     switch (theme) {
       case 'light': return this.setState(themes.LIGHT);
       case 'dark': return this.setState(themes.DARK);
@@ -30,7 +34,11 @@ export default class ReaderSettings extends React.Component {
   
   onSave() {
     this.props.dispatch(setReader(Object.assign(
-      {}, this.state, { annotationsKey: this.refs.annotationsKey.value }
+      {}, (
+        Date.now() > this.props.data.account.subscription
+          ? initialState.config.reader
+          : this.state
+      ), { annotationsKey: this.refs.annotationsKey.getField().value }
     )));
     this.props.dispatch(save('config'));
     
@@ -42,139 +50,178 @@ export default class ReaderSettings extends React.Component {
   }
 
   render() {
+    const { account } = this.props.data;
+
     return (
-      <div className='settings-reader old'>
-        <div className='settings'>
-          <section className='theme'>
-            <label>Reader Theme</label>
-            <select
-              ref='theme'
-              onChange={() => this.onSetTheme()}
-            >
-              <option value='light'>Light</option>
-              <option value='dark'>Dark</option>
-            </select>
-          </section>
+      <div className='reader-settings'>
+        {Date.now() > account.subscription ? (
+          <p>Free users cannot use global reader themes or set custom styling.</p>
+        ) : null}
 
-          <section className='font-size'>
-            <label>Font Size</label>
-            <input
-              type='number'
-              value={this.state.fontSize}
-              min='0.1' step='0.1'
-              onChange={(e) => this.setState({ fontSize: e.target.value })}
-            />
-            <span style={{fontSize: this.state.fontSize + 'em'}}>
-              Example Text
-            </span>
-          </section>
-          
-          <section className='padding'>
-            <label>Page Padding</label>
-            <input
-              type='number'
-              value={this.state.padding}
-              min='0' step='0.1'
-              onChange={(e) => this.setState({ padding: +e.target.value })}
-            />
-            <span
-              className='example'
-              style={{padding: `0em ${1 + this.state.padding}em` }}
-            >Example Text</span>
-          </section>
-          
-          <section className='font-color'>
-            <label>Font Color</label>
-            <input
-              type='color'
-              value={this.state.color}
-              onChange={(e) => this.setState({ color: e.target.value })}
-            />
-            <label>Background Color</label>
-            <input
-              type='color'
-              value={this.state.backgroundColor}
-              onChange={(e) => this.setState({ backgroundColor: e.target.value })}
-            />
-            <span style={{
-              color: this.state.color,
-              backgroundColor: this.state.backgroundColor
-            }}>Example Text</span>
-          </section>
-          
-          <section className='highlight-color'>
-            <label>Highlight / Notes Color</label>
-            <input
-              type='color'
-              value={this.state.highlightColor}
-              onChange={(e) => this.setState({ highlightColor: e.target.value })}
-            />
-            <span style={{
-              color: this.state.color,
-              backgroundColor: this.state.highlightColor
-            }}>Example Text</span>
-          </section>
-          
-          <section className='line-spacing'>
-            <label>Line Spacing</label>
-            <input
-              type='number'
-              value={this.state.lineHeight}
-              min='1' step='0.1'
-              onChange={(e) => this.setState({ lineHeight: e.target.value })}
-            />
-            <div style={{lineHeight: this.state.lineHeight}}>
-              Line 1<br />Line 2<br />Line 3
-            </div>
-          </section>
+        <Paper
+          zDepth={1}
+          component='section'
+          className='theme section flex'
+        >
+          <SelectField
+            id='select--theme'
+            label='Reader Theme'
+            onChange={v => this.onSetTheme(v)}
+            menuItems={[
+              { label: 'Light', value: 'light' },
+              { label: 'Dark', value: 'dark' }
+            ]}
+            className='md-cell'
+          />
 
-          <section className='annotation-color'>
-            <label>Annotation Color</label>
-            <input
-              type='color'
-              value={this.state.annotationColor}
-              onChange={(e) => this.setState({ annotationColor: e.target.value })}
-            />
-            <span style={{
-              color: this.state.color,
-              backgroundColor: this.state.annotationColor
-            }}>Example Text</span>
-          </section>
+          <br />
 
-          <section className='default-highlight-mode'>
-            <label>Default Highlight Mode</label>
-            <select
-              value={this.state.defaultHighlightMode}
-              onChange={(e) => {
-                this.setState({ defaultHighlightMode: e.target.value });
-              }}
-            >
-              <option value='none'>None</option>
-              <option value='notes'>Notes</option>
-              <option value='annotations'>Annotations</option>
-            </select>
-          </section>
+          <TextField
+            id='number--font-size'
+            min={0.1}
+            step={0.1}
+            type='number'
+            label='Font Size'
+            value={this.state.fontSize}
+            onChange={v => this.setState({ fontSize: +v })}
+            className='md-cell'
+          />
 
-          <section className='xyfir-annotations'>
-            <label>Xyfir Annotations Subscription Key</label>
-            <span className='input-description'>
-              A <a href='https://annotations.xyfir.com/' target='_blank'>Xyfir Annotations</a> subscription key is needed to enable <em>annotations mode</em> while reading books.
-            </span>
-            <input
-              ref='annotationsKey'
-              type='text'
-              defaultValue={this.state.annotationsKey}
-            />
-          </section>
-        
-          <button onClick={this.onSave} className='btn-primary'>
-            Save
-          </button>
+          <span
+            className='example font-size'
+            style={{fontSize: this.state.fontSize + 'em'}}
+          >Example Text</span>
+
+          <br />
           
-          <button onClick={this.onReset} className='btn-danger'>
-            Reset
-          </button>
-        </div>
+          <TextField
+            id='number--page-padding'
+            min={0}
+            step={0.1}
+            type='number'
+            label='Page Padding'
+            value={this.state.padding}
+            onChange={v => this.setState({ padding: +v })}
+            className='md-cell'
+          />
+          
+          <span
+            className='example page-padding'
+            style={{padding: `0em ${1 + this.state.padding}em` }}
+          >Example Text</span>
+
+          <br />
+          
+          <ColorPicker
+            id='font-color'
+            label='Font Color'
+            value={this.state.color}
+            onChange={v => this.setState({ color: v })}
+          />
+          
+          <ColorPicker
+            id='background-color'
+            label='Background Color'
+            value={this.state.backgroundColor}
+            onChange={v => this.setState({ backgroundColor: v })}
+          />
+          
+          <span style={{
+            color: this.state.color,
+            backgroundColor: this.state.backgroundColor
+          }}>Example Text</span>
+
+          <br />
+          
+          <ColorPicker
+            id='highlight-color'
+            label='Highlight / Notes Color'
+            value={this.state.highlightColor}
+            onChange={v => this.setState({ highlightColor: v })}
+          />
+          
+          <span style={{
+            color: this.state.color,
+            backgroundColor: this.state.highlightColor
+          }}>Example Text</span>
+          
+          <br />
+
+          <TextField
+            id='number--line-spacing'
+            min={1}
+            step={0.1}
+            type='number'
+            label='Line Spacing'
+            value={this.state.lineHeight}
+            onChange={v => this.setState({ lineHeight: +v })}
+            className='md-cell'
+          />
+          
+          <div style={{lineHeight: this.state.lineHeight}}>
+            Line 1<br />Line 2<br />Line 3
+          </div>
+
+          <br />
+
+          <ColorPicker
+            id='annotation-color'
+            label='Annotation Color'
+            value={this.state.annotationColor}
+            onChange={v => this.setState({ annotationColor: v })}
+          />
+
+          <span style={{
+            color: this.state.color,
+            backgroundColor: this.state.annotationColor
+          }}>Example Text</span>
+
+          <br />
+
+          <SelectField
+            id='select--default-highlight-mode'
+            label='Default Highlight Mode'
+            onChange={v => this.setState({ defaultHighlightMode: v })}
+            menuItems={[
+              { label: 'None', value: 'none' },
+              { label: 'Notes', value: 'notes' },
+              { label: 'Annotations', value: 'annotations' }
+            ]}
+            className='md-cell'
+            defaultValue='none'
+          />
+        </Paper>
+
+        <Paper
+          zDepth={1}
+          component='section'
+          className='xyfir-annotations section flex'
+        >
+          <TextField
+            id='text--xyannotations-key'
+            ref='annotationsKey'
+            type='text'
+            label='Xyfir Annotations Subscription Key'
+            helpText={
+              'A xyAnnotations subscription key is needed to download ' +
+              'annotations while reading a book.'
+            }
+            className='md-cell'
+            defaultValue={this.state.annotationsKey}
+          />
+        </Paper>
+      
+        <Button
+          primary raised
+          onClick={() => this.onSave()}
+          label='Save'
+        >save</Button>
+
+        <Button
+          secondary raised
+          onClick={() => this.onReset()}
+          label='Reset'
+        >clear</Button>
       </div>
     );
   }
