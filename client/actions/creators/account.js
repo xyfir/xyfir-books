6605eeeccbab6940c38804e16e6fc9c7ec29895b
MrXyfir <mr@xyfir.com>
2017-07-26 10:19:12 -0700

    Support SET_XYANNOTATIONS_KEY. Update code style

--- client/actions/creators/account.js
+++ client/actions/creators/account.js
@@ -1,15 +1,21 @@
 import {
-    PURCHASE_SUBSCRIPTION, SET_LIBRARY_SIZE_LIMIT
-} from "../types/account";
+  PURCHASE_SUBSCRIPTION, SET_LIBRARY_SIZE_LIMIT, SET_XYANNOTATIONS_KEY
+} from '../types/account';
 
 export function purchaseSubscription(subscription) {
-    return {
-        type: PURCHASE_SUBSCRIPTION, subscription
-    };
+  return {
+    type: PURCHASE_SUBSCRIPTION, subscription
+  };
 }
 
 export function setLibrarySizeLimit(size) {
-    return {
-        type: SET_LIBRARY_SIZE_LIMIT, size
-    };
+  return {
+    type: SET_LIBRARY_SIZE_LIMIT, size
+  };
+}
+
+export function setXyAnnotationsKey(key) {
+  return {
+    type: SET_XYANNOTATIONS_KEY, key
+  };
 }
--- client/reducers/account.js
+++ client/reducers/account.js
@@ -1,20 +1,25 @@
 import {
-    PURCHASE_SUBSCRIPTION, SET_LIBRARY_SIZE_LIMIT
-} from "actions/types/account";
+  PURCHASE_SUBSCRIPTION, SET_LIBRARY_SIZE_LIMIT, SET_XYANNOTATIONS_KEY
+} from 'actions/types/account';
 
 export default function(state, action) {
-    switch (action.type) {
-        case PURCHASE_SUBSCRIPTION:
-            return Object.assign({}, state, {
-                subscription: action.subscription
-            });
+  switch (action.type) {
+    case PURCHASE_SUBSCRIPTION:
+      return Object.assign({}, state, {
+        subscription: action.subscription
+      });
 
-        case SET_LIBRARY_SIZE_LIMIT:
-            return Object.assign({}, state, {
-                librarySizeLimit: action.size
-            });
-            
-        default:
-            return state;
-    }
+    case SET_LIBRARY_SIZE_LIMIT:
+      return Object.assign({}, state, {
+        librarySizeLimit: action.size
+      });
+    
+    case SET_XYANNOTATIONS_KEY:
+      return Object.assign({}, state, {
+        xyAnnotationsKey: action.key
+      });
+      
+    default:
+      return state;
+  }
 }
