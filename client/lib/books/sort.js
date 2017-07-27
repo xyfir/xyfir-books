d0a16e44a311b5d7f91b0b2d7a0d2db7dfd13ca8
MrXyfir <mr@xyfir.com>
2017-07-26 19:13:23 -0700

    Fix sorting by 'added'. Update code style

--- client/lib/books/sort.js
+++ client/lib/books/sort.js
@@ -1,34 +1,42 @@
-// asc == true for A -> Z, 0 -> 9
+/**
+ * Sorts the books object array.
+ * @param {object[]} books
+ * @param {string} field
+ * @param {boolean} asc
+ * @returns {object[]}
+ */
 export default function(books, field, asc) {
 
-    return books.slice(0).sort((a, b) => {
-        if (a[field] < b[field]) {
-            return asc ? -1 : 1;
-        }
-        else if (a[field] > b[field]) {
-            return asc ? 1 : -1;
-        }
-        // If same series, sort by series_index
-        else if (field == "series") {
-            if (a.series_index < b.series_index)
-                return -1;
-            else if (a.series_index > b.series_index)
-                return 1;
-            else
-                return 0;
-        }
-        // If same authors, sort by title
-        else if (field == "authors" || field == "authors_sort") {
-            if (a.title < b.title)
-                return -1;
-            else if (a.title > b.title)
-                return 1;
-            else
-                return 0;
-        }
-        else {
-            return 0;
-        }
-    });
-    
+  field = field == 'added' ? 'timestamp' : field;
+
+  return books.slice(0).sort((a, b) => {
+    if (a[field] < b[field]) {
+      return asc ? -1 : 1;
+    }
+    else if (a[field] > b[field]) {
+      return asc ? 1 : -1;
+    }
+    // If same series, sort by series_index
+    else if (field == 'series') {
+      if (a.series_index < b.series_index)
+        return -1;
+      else if (a.series_index > b.series_index)
+        return 1;
+      else
+        return 0;
+    }
+    // If same authors, sort by title
+    else if (field == 'authors' || field == 'authors_sort') {
+      if (a.title < b.title)
+        return -1;
+      else if (a.title > b.title)
+        return 1;
+      else
+        return 0;
+    }
+    else {
+      return 0;
+    }
+  });
+  
 }
