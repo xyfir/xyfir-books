2af8119e894cea0d827219fa837b8e3dc276d68c
MrXyfir <mr@xyfir.com>
2017-07-26 19:16:58 -0700

    Use react-md. Update code style

--- client/components/misc/Pagination.jsx
+++ client/components/misc/Pagination.jsx
@@ -1,91 +1,78 @@
-import React from "react";
+import React from 'react';
 
 // Actions
-import { setPage } from "actions/creators/index";
+import { setPage } from 'actions/creators/index';
 
 // Modules
-import parseQuery from "lib/url/parse-hash-query";
+import parseQuery from 'lib/url/parse-hash-query';
+
+// react-md
+import Button from 'react-md/lib/Buttons/Button';
 
 export default class Pagination extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
-    
-    onGoTo(page) {
-        this.props.dispatch(setPage(page));
-    }
+  constructor(props) {
+    super(props);
+  }
+  
+  onGoTo(page) {
+    this.props.dispatch(setPage(page));
+  }
 
-    _buildPagination() {
-        let pagination = {
-            page: this.props.data.search.page, isNextPage: false,
-            isPrevPage: false, isPrevPageFirst: false, isNextPageLast: false,
-            pages: Math.ceil(this.props.items / this.props.itemsPerPage) || 1
-        };
+  _buildPagination() {
+    let pagination = {
+      page: this.props.data.search.page, isNextPage: false,
+      isPrevPage: false, isPrevPageFirst: false, isNextPageLast: false,
+      pages: Math.ceil(this.props.items / this.props.itemsPerPage) || 1
+    };
 
-        // Set boolean values
-        if (pagination.pages > pagination.page)
-            pagination.isNextPage = true;
-        if (pagination.page > 1)
-            pagination.isPrevPage = true;
-        if (pagination.pages == pagination.page + 1)
-            pagination.isNextPageLast = true;
-        if (pagination.isPrevPage && pagination.page - 1 == 1)
-            pagination.isPrevPageFirst = true;
+    // Set boolean values
+    if (pagination.pages > pagination.page)
+      pagination.isNextPage = true;
+    if (pagination.page > 1)
+      pagination.isPrevPage = true;
+    if (pagination.pages == pagination.page + 1)
+      pagination.isNextPageLast = true;
+    if (pagination.isPrevPage && pagination.page - 1 == 1)
+      pagination.isPrevPageFirst = true;
 
-        return pagination;
-    }
+    return pagination;
+  }
 
-    render() {
-        const pagination = this._buildPagination(); 
+  render() {
+    const pagination = this._buildPagination(); 
 
-        return (
-            <div className="pagination">
-                {pagination.isPrevPage && !pagination.isPrevPageFirst ? (
-                    <button
-                        className="btn-secondary first"
-                        onClick={() => this.onGoTo(1)}
-                    >
-                        First Page (1)
-                    </button>
-                ) : (
-                    <span />
-                )}
+    return (
+      <div className='pagination'>
+        {pagination.isPrevPage && !pagination.isPrevPageFirst ? (
+          <Button
+            icon
+            onClick={() => this.onGoTo(1)}
+          >first_page</Button>
+        ) : null}
 
-                {pagination.isPrevPage ? (
-                    <button
-                        className="btn-danger previous"
-                        onClick={() => this.onGoTo(pagination.page - 1)}
-                    >
-                        Previous Page ({pagination.page - 1})
-                    </button>
-                ) : (
-                    <span />
-                )}
-                
-                {pagination.isNextPage ? (
-                    <button
-                        className="btn-primary next"
-                        onClick={() => this.onGoTo(pagination.page + 1)}
-                    >
-                        Next Page ({pagination.page + 1})
-                    </button>
-                ) : (
-                    <span />
-                )}
+        {pagination.isPrevPage ? (
+          <Button
+            icon secondary
+            onClick={() => this.onGoTo(pagination.page - 1)}
+          >navigate_before</Button>
+        ) : null}
+        
+        {pagination.isNextPage ? (
+          <Button
+            icon primary
+            onClick={() => this.onGoTo(pagination.page + 1)}
+          >navigate_next</Button>
+        ) : null}
 
-                {pagination.isNextPage && !pagination.isNextPageLast ? (
-                    <button
-                        className="btn-secondary last"
-                        onClick={() => this.onGoTo(pagination.pages)}
-                    >
-                        Last Page ({pagination.pages})
-                    </button>
-                ) : (
-                    <span />
-                )}
-            </div>
-        );
-    }
+        {pagination.isNextPage && !pagination.isNextPageLast ? (
+          <Button
+            icon
+            onClick={() => this.onGoTo(pagination.pages)}
+          >last_page</Button>
+        ) : null}
+      </div>
+    );
+  }
 
 }
