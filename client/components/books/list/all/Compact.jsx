16277d1262863a99ac9fa1eba89645fa95ace860
MrXyfir <mr@xyfir.com>
2017-07-26 19:15:41 -0700

    Improvements, changes
    - Update code style
    - Use react-md Button
    - Use ul and li
    - Add "You don't have any books!" message

--- client/components/books/list/all/Compact.jsx
+++ client/components/books/list/all/Compact.jsx
@@ -1,115 +1,115 @@
-import React from "react";
+import React from 'react';
 
 // Components
-import Pagination from "components/misc/Pagination";
+import Pagination from 'components/misc/Pagination';
+
+// react-md
+import Button from 'react-md/lib/Buttons/Button';
 
 // Modules
-import findMatches from "lib/books/find-matching";
-import loadCovers from "lib/books/load-covers";
-import parseQuery from "lib/url/parse-hash-query";
-import sortBooks from "lib/books/sort";
-import toUrl from "lib/url/clean";
+import findMatches from 'lib/books/find-matching';
+import loadCovers from 'lib/books/load-covers';
+import parseQuery from 'lib/url/parse-hash-query';
+import sortBooks from 'lib/books/sort';
+import toUrl from 'lib/url/clean';
 
 export default class CompactList extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
+  
+  componentDidMount() {
+    loadCovers(this.props.data.books, this.props.data.account.library);
+  }
+  
+  componentDidUpdate() {
+    loadCovers(this.props.data.books, this.props.data.account.library);
+  }
+
+  render() {
+    const q = parseQuery();
     
-    componentDidMount() {
-        loadCovers(this.props.data.books, this.props.data.account.library);
-    }
+    let books = sortBooks(
+      findMatches(this.props.data.books, this.props.data.search.query),
+      'title', true
+    );
+    const booksCount = books.length;
+
+    if (!booksCount) return <p>You don't have any books!</p>;
     
-    componentDidUpdate() {
-        loadCovers(this.props.data.books, this.props.data.account.library);
-    }
+    books = books.splice((this.props.data.search.page - 1) * 25, 25);
 
-    render() {
-        let q = parseQuery(), books =
-            sortBooks(
-                findMatches(
-                    this.props.data.books, this.props.data.search.query
-                ), "title", true
-            ), booksCount = books.length;
-        books = books.splice((this.props.data.search.page - 1) * 25, 25);
+    return (
+      <div>
+        <ul className='list-compact'>{
+          books.map(b => {
+            const url = `/${b.id}/${toUrl(b.authors)}/${toUrl(b.title)}`;
+            
+            return (
+              <li className='book' key={b.id}>
+                <a href={`#books/read${url}`}>
+                  <img
+                    className='cover'
+                    id={`cover-${b.id}`}
+                  />
+                </a>
+                
+                <div className='info'>
+                  <a
+                    className='title'
+                    href={`#books/read${url}`}
+                  >{b.title}</a>
+                  
+                  <a className='authors' href={
+                    `#books/list/all?search=1&authors=${
+                      encodeURIComponent(b.authors)
+                    }`
+                  }>{b.authors}</a>
+                  
+                  <span className='percent-complete'>{
+                    b.percent_complete + '%'
+                  }</span>
 
-        return (
-            <div>
-                <div className="list-compact">{
-                    books.map(book => {
-                        const url = "/" + book.id + "/" + toUrl(book.authors)
-                            + "/" + toUrl(book.title);
-                        
-                        return (
-                            <div
-                                className="book"
-                                onContextMenu={(e) => {
-                                    e.preventDefault();
-                                    window.location.hash = `#books/manage${url}`;
-                                }}
-                            >
-                                <a href={`#books/read${url}`}>
-                                    <img
-                                        className="cover"
-                                        id={`cover-${book.id}`}
-                                    />
-                                </a>
-                                
-                                <div className="info">
-                                    <a
-                                        className="title"
-                                        href={`#books/read${url}`}
-                                    >{book.title}</a>
-                                    
-                                    <a className="authors" href={
-                                        `#books/list/all?search=1&authors=${
-                                            encodeURIComponent(book.authors)
-                                        }`
-                                    }>{book.authors}</a>
-                                    
-                                    <span className="percent-complete">{
-                                        book.percent_complete + "%"
-                                    }</span>
-                                    {book.word_count > 0 ? (
-                                        <span className="word-count">{
-                                            Math.round(book.word_count / 1000)
-                                        }K</span>
-                                    ) : (
-                                        <span />
-                                    )}
-                                    <span className="date-added">{
-                                        (new Date(book.timestamp))
-                                            .toLocaleDateString()
-                                    }</span>
-                                    {!!(+book.rating) ? (
-                                        <span className="rating">
-                                            <span>{book.rating}</span>
-                                            <span className="icon-star" />
-                                        </span>
-                                    ) : (
-                                        <span />
-                                    )}
-                                </div>
+                  {b.word_count > 0 ? (
+                    <span className='word-count'>{
+                      Math.round(b.word_count / 1000)
+                    }K</span>
+                  ) : null}
 
-                                <div className="controls">
-                                    <a
-                                        href={`#books/manage${url}`}
-                                        className="icon-edit"
-                                    >Manage</a>
-                                </div>
-                            </div>
-                        );
-                    })
-                }</div>
+                  <span className='date-added'>{
+                    (new Date(b.timestamp))
+                      .toLocaleDateString()
+                  }</span>
 
-                <Pagination
-                    itemsPerPage={25}
-                    dispatch={this.props.dispatch}
-                    items={booksCount} 
-                    data={this.props.data}
-                />
-            </div>
-        );
-    }
+                  {!!(+b.rating) ? (
+                    <span className='rating'>
+                      <span>{b.rating}</span>
+                      <span className='icon-star' />
+                    </span>
+                  ) : null}
+                </div>
+
+                <div className='controls'>
+                  <Button
+                    flat
+                    label='Edit'
+                    onClick={() => location.hash = `#books/manage${url}`}
+                  >edit</Button>
+                </div>
+              </li>
+            );
+          })
+        }</ul>
+
+        <Pagination
+          itemsPerPage={25}
+          dispatch={this.props.dispatch}
+          items={booksCount} 
+          data={this.props.data}
+        />
+      </div>
+    );
+  }
 
 }
