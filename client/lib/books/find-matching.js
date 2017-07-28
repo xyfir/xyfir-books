6097daa0a28ec1d57d159281344628c74c1134dc
MrXyfir <mr@xyfir.com>
2017-07-28 08:34:39 -0700

    Improvements, changes
    - Update code style
    - Use JSDOC
    - When searching by specific fields, make sure all of the fields match before matching a book
    - When searching normally, treat each word as its own search (all must match a book before matching)

--- client/lib/books/find-matching.js
+++ client/lib/books/find-matching.js
@@ -1,57 +1,69 @@
+/**
+ * Find and return books that match the search query.
+ * @param {object[]} books
+ * @param {string} query 
+ * @return {object[]}
+ */
 export default function(books, query) {
+  
+  if (query == '') return books;
+  
+  const fieldSearch = /\b(\w+:\w+)\b/.test(query);
+  query = query.toLowerCase().split(' ');
+  
+  return books.filter(book => {
+    const matches = [];
     
-    if (query == '') return books;
-    
-    const search = query.indexOf(':') > -1 ? query.split(' ') : query;
-    
-    return books.filter(book => {
-        let match = false;
+    // Search specific fields
+    if (fieldSearch) {
+      query.forEach(s => {
+        s = s.split(':');
         
-        // Search specific fields
-        if (Array.isArray(search)) {
-            search.forEach(s => {
-                s = s.split(':');
-                
-                if (s[1] === undefined) return;
-                
-                s[1] = s[1].replace(new RegExp('_', 'g'), ' ');
-                
-                if (s[0] == "rating") {
-                    if (s[1] == "unrated") {
-                        if (book.rating === undefined || book.rating == 0)
-                            match = true;
-                        else
-                            match = false;
-                    }
-                    else if (book.rating == s[1].split(' ')[0]) {
-                        match = true;
-                    }
-                    else {
-                        match = false;
-                    }
-                }
-                else if (s[0] == "tag") {
-                    match = !!book.tags.filter(tag => {
-                        return tag.toLowerCase() == s[1];
-                    }).length;
-                }
-                else if (book[s[0]] === undefined) {
-                    match = false;
-                }
-                else if (book[s[0]].toString().toLowerCase().indexOf(s[1]) > -1) {
-                    match = true;
-                }
-            });
-        }
-        // Search title / authors
-        else {
-            if (book.title.toLowerCase().indexOf(search) > -1)
-                match = true;
-            else if (book.authors.toLowerCase().indexOf(search) > -1)
-                match = true;
-        }
+        if (s[1] === undefined) return;
         
-        return match;
-    });
+        s[1] = s[1].replace(/_/g, ' ');
+        
+        if (s[0] == 'rating') {
+          if (s[1] == 'unrated') {
+            if (book.rating === undefined || book.rating == 0)
+              matches.push(true);
+            else
+              matches.push(false);
+          }
+          else if (book.rating == s[1].split(' ')[0]) {
+            matches.push(true);
+          }
+          else {
+            matches.push(false);
+          }
+        }
+        else if (s[0] == 'tag') {
+          matches.push(
+            !!book.tags.filter(tag => tag.toLowerCase() == s[1]).length
+          );
+        }
+        else if (book[s[0]] === undefined) {
+          matches.push(false);
+        }
+        else if (book[s[0]].toString().toLowerCase().indexOf(s[1]) > -1) {
+          matches.push(true);
+        }
+      });
+    }
+    // Treat each word as its own search in both title and authors fields
+    // Only match if each word has a match either in title or authors
+    else {
+      query.forEach(s => {
+        if (book.title.toLowerCase().indexOf(s) > -1)
+          matches.push(true);
+        else if (book.authors.toLowerCase().indexOf(s) > -1)
+          matches.push(true);
+        else
+          matches.push(false);
+      });
+    }
     
+    return query.length == matches.length && matches.findIndex(m => !m) == -1;
+  });
+  
 }
