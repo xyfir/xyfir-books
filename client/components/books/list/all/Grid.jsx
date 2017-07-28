529bd7cae69c9442090635f756b2c716e9a09dd0
MrXyfir <mr@xyfir.com>
2017-07-27 17:49:26 -0700

    Improve GridList output and code

--- client/components/books/list/all/Grid.jsx
+++ client/components/books/list/all/Grid.jsx
@@ -7,13 +7,19 @@ import Pagination from 'components/misc/Pagination';
 import findMatches from 'lib/books/find-matching';
 import loadCovers from 'lib/books/load-covers';
 import parseQuery from 'lib/url/parse-hash-query';
+import deleteBook from 'lib/books/delete';
 import sortBooks from 'lib/books/sort';
-import toUrl from 'lib/url/clean';
+import buildUrl from 'lib/url/build';
+
+// react-md
+import Button from 'react-md/lib/Buttons/Button';
 
 export default class GridList extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = { selected: -1 };
   }
   
   componentDidMount() {
@@ -40,32 +46,46 @@ export default class GridList extends React.Component {
     return (
       <div>
         <ul className='list-grid'>{
-          books.map(b => {
-            const url = `/${b.id}/${toUrl(b.authors)}/${toUrl(b.title)}`;
-            
-            return (
-              <li className='book' key={b.id}>
-                <a href={`#books/read${url}`}>
-                  <img
-                    className='cover'
-                    id={`cover-${b.id}`}
-                  />
-                </a>
-                
-                <div className='info'>
-                  <a
-                    className='title'
-                    href={`#books/read${url}`}
-                  >{b.title}</a>
-                  <a className='authors' href={
-                    `#books/list/all?search=1&authors=${
-                      encodeURIComponent(b.authors)
-                    }`
-                  }>{b.authors}</a>
+          books.map(b =>
+            <li
+              className='book'
+              onClick={() => this.setState({ selected: b.id })}
+              key={b.id}
+            >
+              {this.state.selected == b.id ? (
+                <div className='overlay'>
+                  <Button
+                    flat
+                    label='Read'
+                    onClick={() => location.hash = buildUrl(b, 'read')}
+                  >book</Button>
+                  <Button
+                    flat
+                    label='Metadata'
+                    onClick={() => location.hash = buildUrl(b, 'manage')}
+                  >edit</Button>
+                  <Button
+                    flat
+                    label='Search author(s)'
+                    onClick={() => location.hash = buildUrl(b, 'authors')}
+                  >person</Button>
+                  <Button
+                    flat
+                    label='Delete'
+                    onClick={() => deleteBook([b.id], this.props.dispatch)}
+                  >delete</Button>
                 </div>
-              </li>
-            );
-          })
+              ) : null}
+              
+              <img
+                className='cover'
+                id={`cover-${b.id}`}
+              />
+              
+              <span className='title'>{b.title}</span>
+              <span className='authors'>{b.authors}</span>
+            </li>
+          )
         }</ul>
 
         <Pagination
