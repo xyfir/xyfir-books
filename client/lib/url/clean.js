044cfa9b53c22e55a1c4362d23da7a7eac79ea1a
MrXyfir <mr@xyfir.com>
2017-07-27 17:48:28 -0700

    Update code style

--- client/lib/url/clean.js
+++ client/lib/url/clean.js
@@ -1,9 +1,13 @@
+/**
+ * Cleans an input to become part of a url.
+ * @param {string} input
+ * @return {string}
+ */
 export default function(input) {
-    
-    return input.replace(
-        new RegExp("[^0-9a-zA-Z ]", "g"), ""
-    ).replace(
-        new RegExp(" ", "g"), "-"
-    ).substr(0, 30);
-    
+
+  return input
+    .replace(new RegExp('[^0-9a-zA-Z ]', 'g'), '')
+    .replace(new RegExp(' ', 'g'), '-')
+    .substr(0, 40);
+
 }
