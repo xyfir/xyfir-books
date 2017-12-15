d8bcfe15907b04e354d30f862e2c21e100129909
MrXyfir <mr@xyfir.com>
2017-12-15 12:52:36 -0800

    Updates and improvements
    - Use react-md
    - Use superagent
    - Update code style

--- client/components/books/AddFormat.jsx
+++ client/components/books/AddFormat.jsx
@@ -1,13 +1,11 @@
-import swal from 'sweetalert';
-import React from 'react';
+import { Paper, Button, TextField, SelectField } from 'react-md';
 import Dropzone from 'react-dropzone';
+import request from 'superagent';
+import React from 'react';
+import swal from 'sweetalert';
 
 // Action creators
 import { addFormat } from 'actions/creators/books';
-
-// Modules
-import request from 'lib/request/index';
-import upload from 'lib/request/upload';
 
 // Constants
 import { LIBRARY } from 'constants/config';
@@ -21,123 +19,129 @@ export default class AddFormat extends React.Component {
       id: window.location.hash.split('/')[3],
       converting: false, uploading: false
     };
-    
-    this.onConvert = this.onConvert.bind(this);
-    this.onUpload = this.onUpload.bind(this);
   }
-  
+
   onConvert() {
-    if (!navigator.onLine) {
-      swal('Error', 'This action requires internet connectivity', 'error');
-      return;
-    }
-    
+    if (!navigator.onLine)
+      return swal('Error', 'Internet connectivity required', 'error');
+
     this.setState({ converting: true });
-    
-    const url = LIBRARY +
-      'libraries/' + this.props.data.account.library +
-      '/books/' + this.state.id +
-      '/format/convert?from=' + this.refs.convertFrom.value +
-      '&to=' + this.refs.convertTo.value;
-      
-    request({url, method: 'POST'}, res => {
-      this.setState({ converting: false });
-      
-      if (res.error) {
-        swal('Error', 'Could not convert format', 'error');
-      }
-      else {
-        this.props.dispatch(addFormat(
-          this.state.id, this.refs.convertTo.value
-        ));
+
+    request
+      .post(
+        `${LIBRARY}libraries/${this.props.data.account.library}` +
+        `/books/${this.state.id}/format/convert`
+      )
+      .query({
+        from: this._convertFrom.value,
+        to: this._convertTo.value
+      })
+      .end((err, res) => {
+        this.setState({ converting: false });
+        
+        if (err || res.body.error)
+          return swal('Error', 'Could not convert format', 'error');
+
+        this.props.dispatch(
+          addFormat(this.state.id, this._convertTo.value)
+        );
         swal('Success', 'Format added', 'success');
-      }
-    });
+      });
   }
   
   onUpload(files) {
-    if (!navigator.onLine) {
-      swal('Error', 'This action requires internet connectivity', 'error');
-      return;
-    }
-    
+    if (!navigator.onLine)
+      return swal('Error', 'Internet connectivity required', 'error');
+
     this.setState({ upload: true });
-    
-    const url = LIBRARY +
-      'libraries/' + this.props.data.account.library +
-      '/books/' + this.state.id + '/format'; 
-    
-    upload(url, 'POST', 'book', [files[0]], res => {
-      this.setState({ upload: false });
-      
-      if (res.error) {
-        swal('Error', 'Could not upload file', 'error');
-      }
-      else {
-        this.props.dispatch(addFormat(
-          this.state.id, files[0].name.split('.').pop()
-        ));
+
+    request
+      .post(
+        `${LIBRARY}libraries/${this.props.data.account.library}` +
+        `/books/${this.state.id}/format`
+      )
+      .attach('book', files[0])
+      .end((err, res) => {
+        this.setState({ upload: false });
+        
+        if (err || res.body.error) {
+          console.log('err', err, 'res', res.body);
+          return swal('Error', 'Could not upload file', 'error');
+        }
+
+        this.props.dispatch(
+          addFormat(this.state.id, files[0].name.split('.').pop())
+        );
         swal('Success', 'Format added', 'success');
-      }
-    });
+      });
   }
 
   render() {
     const book = this.props.data.books.find(b => this.state.id == b.id);
-    const formats = book.formats.map(format => {
-      return format.split('.').slice(-1)[0].toUpperCase();
-    });
+    const formats = book.formats.map(format =>
+      format.split('.').slice(-1)[0].toUpperCase()
+    );
     
     return (
-      <div className='add-format old'>
+      <div className='add-format'>
         <p>
-          <strong>Note:</strong> Only <a href='https://en.wikipedia.org/wiki/EPUB' target='_blank'>EPUB</a> format ebooks can be read by Xyfir Books' ebook reader.
+          <strong>Note:</strong> Only <a href='https://en.wikipedia.org/wiki/EPUB' target='_blank'>EPUB</a> format ebooks can be read directly in the xyBooks ebook reader.
         </p>
         
-        <section className='upload'>
+        <Paper
+          zDepth={1}
+          component='section'
+          className='upload section flex'
+        >
           <h2>Upload</h2>
           <p>
-            Add a format a new format for <strong>{book.title}</strong>. If you upload a format that already exists, the old file will be replaced.
+            Add a new format for <strong>{book.title}</strong>. If you upload a format that already exists, the old file will be replaced.
           </p>
           <p><strong>Current Available Formats:</strong> {formats.join(', ')}</p>
-          
-          <hr />
-          
-          <Dropzone onDrop={this.onUpload} className='dropzone'>{
-            this.state.uploading ? (
-              'Uploading file, please wait...'
-            ) : (
-              'Drag and drop ebook file or click box to choose file to upload.'
-            )
+
+          <Dropzone onDrop={f => this.onUpload(f)} className='dropzone'>{
+            this.state.uploading
+              ? 'Uploading file, please wait...'
+              : 'Drag and drop ebook or click box to choose a file to upload.'
           }</Dropzone>
-        </section>
+        </Paper>
         
-        <section className='convert'>
+        <Paper
+          zDepth={1}
+          component='section'
+          className='convert section flex'
+        >
           <h2>Convert Format</h2>
           <p>
-            Our system can attempt to automatically convert an already existing format to a different format. This process is not perfect and can cause formatting and other issues.
-            <br />
-            The original format will remain untouched.
+            Our system can attempt to automatically convert an already existing format to a different format. This process is not perfect and may cause issues within the new format.
           </p>
+          <p>The original format will remain untouched.</p>
+
+          <SelectField
+            id='select--convert-from'
+            ref={i => this._convertFrom = i}
+            label='Convert From'
+            menuItems={
+              formats.map(format =>
+                Object({ label: format, value: format.toLowerCase() })
+              )
+            }
+            className='md-cell'
+          />
+
+          <TextField
+            id='text--convert-to'
+            ref={i => this._convertTo = i}
+            type='text'
+            label='Convert to Format'
+            className='md-cell'
+          />
           
-          <hr />
-          
-          <label>Convert From</label>
-          <select ref='convertFrom'>{
-            formats.map(format =>
-              <option value={format.toLowerCase()}>{
-                format
-              }</option>
-            )
-          }</select>
-          
-          <label>Convert To</label>
-          <input type='text' ref='convertTo' placeholder='Format' />
-          
-          <button className='btn-secondary' onClick={this.onConvert}>{
-            this.state.converting ? 'Converting...' : 'Convert'
-          }</button>
-        </section>
+          <Button
+            raised primary
+            onClick={() => this.onConvert()}
+          >{this.state.converting ? 'Converting...' : 'Convert'}</Button>
+        </Paper>
       </div>
     );
   }
