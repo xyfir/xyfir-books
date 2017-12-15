b9b9e4099407c6c17a77ccf5fc94662b28ed1b66
MrXyfir <mr@xyfir.com>
2017-12-15 13:31:12 -0800

    Updates, improvements
    - Use react-md
    - Use supergent
    - Update code style

--- client/components/books/BulkEdit.jsx
+++ client/components/books/BulkEdit.jsx
@@ -1,10 +1,11 @@
-import swal from 'sweetalert';
-import React from 'react';
+import { Paper, Button, TextField, SelectField, Checkbox } from 'react-md';
 import titleCase from 'titlecase';
+import request from 'superagent';
+import React from 'react';
+import swal from 'sweetalert';
 
 // Modules
 import loadBooks from 'lib/books/load-from-api';
-import request from 'lib/request/index';
 
 // Constants
 import { LIBRARY } from 'constants/config';
@@ -15,240 +16,291 @@ export default class EditBooks extends React.Component {
     super(props);
     
     this.state = {
-      ids: window.location.hash.split('/')[3].split(','),
+      ids: location.hash.split('/')[3].split(',').map(Number),
       status: ''
     };
-    
-    this.onSaveChanges = this.onSaveChanges.bind(this);
   }
   
-  onSaveChanges(e) {
-    e.preventDefault();
+  async onSaveChanges() {
+    if (!navigator.onLine)
+      return swal('Error', 'Internet connectivity needed', 'error');
 
-    if (!navigator.onLine) {
-      swal('Error', 'This action requires internet connectivity', 'error');
-      return;
-    }
-    
     this.setState({ status: 'Allocating space on server' });
-    
-    const r = this.refs;
 
-    const updateBook = (index) => {
-      const id = this.state.ids[index];
-      
-      // All books updated
-      if (id === undefined) {
-        this.setState({ status: '' });
-        swal('Action Complete', '', 'success');
-        
-        // Update state.books and local storage
-        loadBooks(
-          this.props.data.account.library,
-          this.props.dispatch
-        );
-        
-        return;
-      }
-      
+    for (let id of this.state.ids) {
       const book = this.props.data.books.find(b => id == b.id);
       
       // Skip book or abort editing
-      if (book === undefined) {
-        if (r.abort_on_error.checked) {
-          this.setState({ status: `Could not find book with id '${id}'. Aborting...` });
-          return;
-        }
-        else {
-          this.setState({ status: `Could not find book with id '${id}'. Skipping...` });
-          updateBook(index + 1);
-          return;
-        }
+      if (!book) {
+        this.setState({ status: `Could not find book with id '${id}'.` });
+
+        if (window['checkbox--stop-on-error'].checked) return;
+        else continue;
       }
-      else {
-        this.setState({ status: `Editing book '${book.title || id}'` });
-      }
-      
+
+      this.setState({ status: `Editing book '${book.title || id}'` });
+
       // Build data object to send to server
-      let data = {};
+      const data = {};
       
       // Swap authors / title
-      if (r.author_title_swap.checked) {
+      if (window['checkbox--author-title-swap'].checked) {
         data.authors = book.title;
         data.title = book.authors;
       }
       // Set authors
-      if (r.authors.value != '') {
-        data.authors = r.authors.value;
+      if (this._authors.value != '') {
+        data.authors = this._authors.value;
       }
       // Format title
-      if (r.title_format.checked) {
+      if (window['checkbox--format-title'].checked) {
         data.title = titleCase(data.title || book.title);
       }
       // Rating
-      if (r.rating.value != '') {
-        data.rating = +r.rating.value;
+      if (this._rating.value != '') {
+        data.rating = +this._rating.value;
         
-        if (data.rating > 0) {
-          data.rating = data.rating / 2;
-        }
+        if (data.rating > 0) data.rating = data.rating / 2;
       }
       // Publisher
-      if (r.publisher.value != '') {
-        data.publisher = r.publisher.value;
+      if (this._publisher.value != '') {
+        data.publisher = this._publisher.value;
       }
       // Published
-      if (r.published.value != '') {
-        data.pubdate = (new Date(r.published.value)).toISOString();
+      if (this._published.value != '') {
+        data.pubdate = (new Date(this._published.value)).toISOString();
       }
       // Set data.tags
-      if (r.add_tags.value != '' || r.rem_tags.value != '') {
+      if (this._addTags.value != '' || this._remTags.value != '') {
         data.tags = book.tags;
-        
+
         // Add tags
-        if (r.add_tags.value != '') {
-          data.tags = data.tags.concat(r.add_tags.value.split(', '));
+        if (this._addTags.value != '') {
+          data.tags = data.tags.concat(this._addTags.value.split(', '));
         }
         // Remove tags
-        if (r.rem_tags.value != '') {
-          const remove = r.rem_tags.value.toLowerCase().split(', ');
+        if (this._remTags.value != '') {
+          const remove = this._remTags.value.toLowerCase().split(', ');
           
-          data.tags = data.tags.filter(t1 => {
-            return remove.indexOf(t1.toLowerCase()) == -1;
-          });
+          data.tags = data.tags.filter(t1 =>
+            remove.indexOf(t1.toLowerCase()) == -1
+          );
         }
-        
+
         data.tags = data.tags.join(', ');
       }
       // Clear tags
-      if (r.clear_tags.checked) {
+      if (window['checkbox--clear-tags'].checked) {
         data.tags = '';
       }
       // Set series
-      if (r.series.value != '') {
-        data.series = r.series.value;
+      if (this._series.value != '') {
+        data.series = this._series.value;
       }
       // Clear series
-      if (r.clear_series.checked) {
+      if (window['checkbox--clear-series'].checked) {
         data.series = '';
       }
       // Date added
-      if (r.timestamp.value != '') {
-        data.timestamp = (new Date(r.timestamp.value)).toISOString();
+      if (this._timestamp.value != '') {
+        data.timestamp = (new Date(this._timestamp.value)).toISOString();
       }
       // Comments
-      if (r.comments.value != '') {
-        data.comments = r.comments.value;
+      if (this._comments.value != '') {
+        data.comments = this._comments.value;
       }
-      
+
       this.setState({ status: 'Sending new metadata to server' });
-      
-      const url = LIBRARY +
-        'libraries/' + this.props.data.account.library +
-        '/books/' + id + '/metadata';
-      
+
       // Update book's metadata
-      request({url, method: 'PUT', data: { data: JSON.stringify(data) }, success: (res) => {
-        if (res.error) {
-          if (r.abort_on_error.checked) {
-            this.setState({
-              status: `Error updating book '${data.title || book.title || id}'. Aborting...`
-            });
-            return;
-          }
-          else {
-            this.setState({
-              status: `Error updating book '${data.title || book.title || id}'.`
-            });
-            updateBook(index + 1);
-          }
+      try {
+        const res = await request
+          .put(
+            `${LIBRARY}libraries/${this.props.data.account.library}/books/` +
+            `${id}/metadata`
+          )
+          .send({
+            data: JSON.stringify(data)
+          });
+
+        if (res.body.error) throw res.body.error;
+      }
+      catch (err) {
+        if (window['checkbox--stop-on-error'].checked) {
+          return this.setState({
+            status: `Error updating '${data.title || book.title || id}'.`
+          });
         }
         else {
-          updateBook(index + 1);
+          this.setState({
+            status: `Error updating '${data.title || book.title || id}'.`
+          });
+          continue;
         }
-      }})
-    };
-    
-    updateBook(0);
+      }
+    }
+
+    this.setState({ status: '' });
+    swal('Action Complete', '', 'success');
+
+    // Update state.books and local storage
+    loadBooks(
+      this.props.data.account.library,
+      this.props.dispatch
+    );
   }
 
   render() {
     return (
-      <div className='edit-books old'>
-        <form onSubmit={(e) => this.onSaveChanges(e)}>
-          <section>
-            <label>Authors</label>
-            <input type='text' ref='authors' />
-            
-            <span className='checkbox'>
-              <input type='checkbox' ref='author_title_swap' />
-              Swap Title / Authors
-            </span>
-            
-            <label>Author Sort</label>
-            <input type='text' ref='author_sort' />
-          </section>
+      <div className='edit-books'>
+        <Paper
+          zDepth={1}
+          component='section'
+          className='authors section flex'
+        >
+          <TextField
+            id='text--authors'
+            ref={i => this._authors = i}
+            type='text'
+            label='Authors'
+            className='md-cell'
+          />
+
+          <TextField
+            id='text--author-sort'
+            ref={i => this._authorSort = i}
+            type='text'
+            label='Author Sort'
+            className='md-cell'
+          />
+
+          <Checkbox
+            id='checkbox--author-title-swap'
+            label='Swap Title / Authors'
+          />
+        </Paper>
+        
+        <Paper
+          zDepth={1}
+          component='section'
+          className='publish section flex'
+        >
+          <TextField
+            id='text--publisher'
+            ref={i => this._publisher = i}
+            type='text'
+            label='Publisher'
+            className='md-cell'
+          />
+
+          <TextField
+            id='text--published'
+            ref={i => this._published = i}
+            type='text'
+            label='Published (Date)'
+            className='md-cell'
+            placeholder='YYYY-MM-DD'
+          />
+        </Paper>
           
-          <section>
-            <label>Publisher</label>
-            <input type='text' ref='publisher' />
-            
-            <label>Published</label>
-            <input type='date' ref='published' />
-          </section>
-            
-          <section>
-            <label>Add Tags</label>
-            <input type='text' ref='add_tags' />
-            
-            <label>Remove Tags</label>
-            <input type='text' ref='rem_tags' />
-            <input type='checkbox' ref='clear_tags' />Clear Tags
-          </section>
-          
-          <section>
-            <label>Rating</label>
-            <input type='number' ref='rating' max='5' step='0.5' />
-            
-            <label>Series</label>
-            <input type='text' ref='series' />
-            <input type='checkbox' ref='clear_series' />Clear Series
-            
-            <label>Added</label>
-            <input type='date' ref='timestamp' />
-            
-            <label>Comments</label>
-            <textarea ref='comments' className='comments-edit' />
-            
-            <span className='checkbox'>
-              <input type='checkbox' ref='title_format' />
-              Convert Title to Title Case
-            </span>
-            
-            <span className='checkbox'>
-              <input type='checkbox' ref='abort_on_error' />
-              Stop Editing on Error
-            </span>
-          </section>
-          
-          <section>
-            <p>
-              <strong>Note:</strong> Fields left blank will not be changed
-            </p>
-            
-            <button className='btn-primary'>
-              Save Changes
-            </button>
-            
-            <div className='status'>{this.state.status ? (
-              <p>
-                <strong>Status:</strong> {this.state.status}
-              </p>
-            ) : (
-              <span />
-            )}</div>
-          </section>
-        </form>
+        <Paper
+          zDepth={1}
+          component='section'
+          className='tags section flex'
+        >
+          <TextField
+            id='text--add-tags'
+            ref={i => this._addTags = i}
+            type='text'
+            label='Add Tags'
+            className='md-cell'
+          />
+
+          <TextField
+            id='text--remove-tags'
+            ref={i => this._remTags = i}
+            type='text'
+            label='Remove Tags'
+            className='md-cell'
+          />
+
+          <Checkbox
+            id='checkbox--clear-tags'
+            label='Clear Tags'
+          />
+        </Paper>
+        
+        <Paper
+          zDepth={1}
+          component='section'
+          className='misc section flex'
+        >
+          <TextField
+            id='number--rating'
+            max={5}
+            ref={i => this._rating = i}
+            step={0.5}
+            type='number'
+            label='Rating'
+            className='md-cell'
+          />
+
+          <TextField
+            id='text--series'
+            ref={i => this._series = i}
+            type='text'
+            label='Series'
+            className='md-cell'
+          />
+
+          <Checkbox
+            id='checkbox--clear-series'
+            label='Clear Series'
+          />
+
+          <TextField
+            id='text--timestamp'
+            ref={i => this._timestamp = i}
+            type='text'
+            label='Date Added'
+            className='md-cell'
+            placeholder='YYYY-MM-DD'
+          />
+
+          <TextField
+            id='text--comments'
+            ref={i => this._comments = i}
+            rows={2}
+            type='text'
+            label='Comments'
+            className='md-cell'
+          />
+
+          <Checkbox
+            id='checkbox--format-title'
+            label='Convert Title to Title Case'
+          />
+
+          <Checkbox
+            id='checkbox--stop-on-error'
+            label='Stop Editing on Error'
+            defaultChecked={true}
+          />
+        </Paper>
+
+        <p><strong>Note:</strong> Fields left blank will not be changed</p>
+
+        {this.state.status ? (
+          <p className='status'>
+            <strong>Status:</strong> {this.state.status}
+          </p>
+        ) : null}
+
+        <Button
+          primary raised
+          iconChildren='save'
+          onClick={() => this.onSaveChanges()}
+        >Save Changes</Button>
       </div>
     );
   }
