0d21eb9ca51d9713bed4652426f90397917b22b9
MrXyfir <mr@xyfir.com>
2017-12-18 12:42:29 -0800

    Update code style. Support string array in save()

--- client/actions/creators/index.js
+++ client/actions/creators/index.js
@@ -1,27 +1,23 @@
 import {
-    CHANGE_VIEW, SET_SEARCH, SET_SAVE, SET_PAGE
-} from "../types/index";
+  CHANGE_VIEW, SET_SEARCH, SET_SAVE, SET_PAGE
+} from 'actions/types/index';
 
+/** @param {string} view */
 export function changeView(view) {
-    return {
-        type: CHANGE_VIEW, view
-    };
+  return { type: CHANGE_VIEW, view };
 }
 
+/** @param {string} query */
 export function setSearch(query) {
-    return {
-        type: SET_SEARCH, query
-    };
+  return { type: SET_SEARCH, query };
 }
 
+/** @param {number} page */
 export function setPage(page) {
-    return {
-        type: SET_PAGE, page
-    };
+  return { type: SET_PAGE, page };
 }
 
+/** @param {string|string[]} prop */
 export function save(prop) {
-    return {
-        type: SET_SAVE, prop
-    };
+  return { type: SET_SAVE, prop: Array.isArray(prop) ? prop : [prop] };
 }
