040dc35a16f362d010c5b31253945203a13811b7
MrXyfir <mr@xyfir.com>
2017-12-30 11:02:11 -0800

    Fixes. Update code style

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,50 +1,52 @@
 // Reducers
-import settings from "./settings";
-import account from "./account";
-import books from "./books";
+import settings from 'reducers/settings';
+import account from 'reducers/account';
+import books from 'reducers/books';
 
 import {
-    INITIALIZE_STATE, CHANGE_VIEW, SET_SEARCH, SET_SAVE, SET_PAGE
-} from "../actions/types/index";
+  INITIALIZE_STATE, CHANGE_VIEW, SET_SEARCH, SET_SAVE, SET_PAGE
+} from 'actions/types/index';
 
 export default function (state, action) {
-        
-    switch (action.type) {
-        case INITIALIZE_STATE:
-            return action.state;
-            
-        case CHANGE_VIEW:
-            return Object.assign({}, state, { view: action.view });
-            
-        case SET_SEARCH:
-            return Object.assign({}, state, {
-                search: Object.assign({}, state.search, {
-                    query: action.query
-                })
-            });
 
-        case SET_PAGE:
-            return Object.assign({}, state, {
-                search: Object.assign({}, state.search, {
-                    page: action.page
-                })
-            });
-            
-        case SET_SAVE:
-            return Object.assign({}, state, { save: action.prop });
-            
-        default:
-            if (state === undefined) {
-                return {};
-            }
-            else {
-                return {
-                    account: account(state.account, action),
-                    config: settings(state.config, action),
-                    books: books(state.books, action),
-                    view: state.view
-                };
-            }
-    }
+  switch (action.type) {
+    case INITIALIZE_STATE:
+      return action.state;
+
+    case CHANGE_VIEW:
+      return Object.assign({}, state, { view: action.view });
+
+    case SET_SEARCH:
+      return Object.assign({}, state, {
+        search: Object.assign({}, state.search, {
+          query: action.query
+        })
+      });
+
+    case SET_PAGE:
+      return Object.assign({}, state, {
+        search: Object.assign({}, state.search, {
+          page: action.page
+        })
+      });
+
+    case SET_SAVE:
+      return Object.assign({}, state, { save: action.prop });
+
+    default:
+      if (state === undefined) {
+        return {};
+      }
+      else {
+        return {
+          account: account(state.account, action),
+          config: settings(state.config, action),
+          search: state.search,
+          books: books(state.books, action),
+          view: state.view,
+          save: state.save
+        };
+      }
+  }
 
 }
