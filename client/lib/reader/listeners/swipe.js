8a619bd6c469a4623e3c38520bcad9877f263884
MrXyfir <mr@xyfir.com>
2018-01-04 12:19:21 -0800

    Fix converting taps to clicks

--- client/lib/reader/listeners/swipe.js
+++ client/lib/reader/listeners/swipe.js
@@ -5,14 +5,14 @@
  * @param {function} fn - The listener function.
  */
 export default function(el, book, fn) {
-  
+
   // Required min distance traveled to be considered swipe
   const threshold   = 100;
   // Maximum time allowed to travel that distance
   const allowedTime = 350;
   // Maximum distance allowed at the same time in perpendicular direction
   const restraint   = 100;
-  
+
   let startX, startY, startTime;
 
   el.addEventListener('touchstart', e => {
@@ -20,15 +20,15 @@ export default function(el, book, fn) {
     startY = e.changedTouches[0].pageY;
     startTime = Date.now();
   }, false);
-  
+
   el.addEventListener('touchend', e => {
     // Get distance traveled by finger while in contact with surface
     const distX = e.changedTouches[0].pageX - startX;
     const distY = e.changedTouches[0].pageY - startY;
-    
+
     // Time elapsed since touchstart
     const elapsedTime = Date.now() - startTime;
-    
+
     if (elapsedTime <= allowedTime) {
       // Horizontal swipe
       if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint)
@@ -40,19 +40,19 @@ export default function(el, book, fn) {
         fn((distY < 0) ? 'up' : 'down');
       // Tap
       else {
-        const [{document, documentElement}] = book.rendition.getContents();
+        const [{document}] = book.rendition.getContents();
 
         document.defaultView.getSelection().removeAllRanges();
 
         // Convert tap to click
-        documentElement.dispatchEvent(
+        document.dispatchEvent(
           new MouseEvent('click', {
             clientX: startX, clientY: startY
           })
         );
 
-        // !! Needed when emulating touch events to prevent double 'clicks'
-        //e.preventDefault();
+        // !! Needed to prevent double 'clicks' in certain environments
+        e.preventDefault();
       }
     }
   }, false);
