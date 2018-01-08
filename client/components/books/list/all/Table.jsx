ed33b0c7345cf202e72a194dc77d4c73b0fdc700
MrXyfir <mr@xyfir.com>
2018-01-07 16:26:46 -0800

    Update table portion

--- client/components/books/list/all/Table.jsx
+++ client/components/books/list/all/Table.jsx
@@ -1,4 +1,7 @@
-import { FontIcon, Button, Divider } from 'react-md';
+import {
+  FontIcon, Button, Divider, DataTable, TableHeader, TableBody, TableRow,
+  TableColumn
+} from 'react-md';
 import request from 'superagent';
 import moment from 'moment';
 import React from 'react';
@@ -21,13 +24,14 @@ export default class TableList extends React.Component {
 
     const {state: AppState} = this.props.App;
 
-    const selected = AppState.books.length
-      ? [rand(0, AppState.books.length - 1)]
-      : [];
-
     this.state = {
-      selected, sort: AppState.config.bookList.table.defaultSort
-    };
+      selected: AppState.books.length
+        ? [rand(0, AppState.books.length - 1)]
+        : [],
+      sort: AppState.config.bookList.table.defaultSort
+    },
+    /** @prop {object[]} The books that are currently rendered */
+    this.books = [];
   }
 
   componentDidMount() {
@@ -40,22 +44,54 @@ export default class TableList extends React.Component {
     loadCovers(books, account.library);
   }
 
-  onSelect(e, id) {
+  /**
+   * Called whenever a row is clicked. Only used for [un]selecting all.
+   * @param {number} id - `0` if all rows.
+   * @param {boolean} checked
+   */
+  onRowToggle(id, checked) {
+    if (id !== 0) return;
+
+    // Unselect all rows
+    if (!checked)
+      this.setState({ selected: [] });
+    // Select all VISIBLE rows
+    else
+      this.setState({ selected: this.books.map(b => b.id) });
+  }
+
+  /**
+   * Handle a row being clicked.
+   * @param {Event} e
+   * @param {number} id - The id of the book whose row was clicked
+   */
+  onRowClick(e, id) {
+    const node = e.target.nodeName;
+
+    if (node == 'I' || node == 'INPUT') return;
+
+    this.onSelect(id, true, true);
+  }
+
+  /**
+   * Handles [un]selecting a book row.
+   * @param {number} id - The id of the selected book
+   * @param {boolean} select - `true` if it was selected, `false` if unselected
+   * @param {boolean} [reset=false] - If `true` it unselects all books other
+   *  than this one. When `true`, `select` is also assumed to be `true`.
+   */
+  onSelect(id, select, reset = false) {
     const {selected} = this.state;
 
-    // Select multiple items
-    if (e.ctrlKey) {
-      // Add item
-      if (selected.indexOf(id) == -1)
-        this.setState({ selected: selected.concat([id]) });
-      // Remove item
-      else if (this.state.selected.length > 1)
-        this.setState({ selected: selected.filter(s => s != id) });
-    }
-    // Select single item
-    else {
+    // Only select this book
+    if (reset)
       this.setState({ selected: [id] });
-    }
+    // Unselect book
+    else if (!select)
+      this.setState({ selected: selected.filter(s => s != id) });
+    // Select book if not already
+    else if (selected.indexOf(id) == -1)
+      this.setState({ selected: selected.concat([id]) });
   }
 
   onDelete() {
@@ -75,6 +111,83 @@ export default class TableList extends React.Component {
     // Change state.sort.column, asc always true
     else
       this.setState({ sort: { column, asc: true } });
+  }
+
+  /** @return {JSX.Element} */
+  _renderTableHeaderRow() {
+    const {columns} = this.props.App.state.config.bookList.table;
+    const {sort} = this.state;
+
+    return (
+      <TableRow>{
+        columns.map(col =>
+          <TableColumn
+            onClick={() => this.onSort(col)}
+            sorted={sort.column == col ? sort.asc : undefined}
+            key={col}
+          >{col.replace(/\b[a-z]/g, c => c.toUpperCase())}</TableColumn>
+        )
+      }</TableRow>
+    );
+  }
+
+  /**
+   * @param {object} book
+   * @return {JSX.Element}
+   */
+  _renderTableBodyRow(book) {
+    const {selected} = this.state;
+    const {columns} = this.props.App.state.config.bookList.table;
+
+    return (
+      <TableRow
+        onCheckboxClick={(i, checked, e) => this.onSelect(book.id, checked)}
+        selected={selected.indexOf(book.id) > -1}
+        onClick={e => this.onRowClick(e, book.id)}
+        key={book.id}
+      >{
+        columns.map(col => {
+          switch (col) {
+            case 'added': return (
+              <TableColumn className='added' key={col}>{
+                moment(book.timestamp).format('YYYY-MM-DD')
+              }</TableColumn>
+            )
+
+            case 'rating': return (
+              <TableColumn className='rating' key={col}>{
+                book.rating !== undefined ? (
+                  <React.Fragment>
+                    {book.rating}
+                    <FontIcon>stars</FontIcon>
+                  </React.Fragment>
+                ) : 'None'
+              }</TableColumn>
+            )
+
+            case 'published': return (
+              <TableColumn className='published' key={col}>{
+                moment(book.pubdate).format('YYYY-MM-DD')
+              }</TableColumn>
+            )
+
+            case 'series': return (
+              <TableColumn className='series' key={col}>{
+                !book.series
+                  ? ''
+                  : `${book.series} [${book.series_index}]`
+              }</TableColumn>
+            )
+
+            default: return (
+              <TableColumn className={col} key={col}>
+                {book[col]}
+              </TableColumn>
+            )
+          }
+        })
+      }</TableRow>
+    );
   }
 
   /** @return {JSX.Element} */
@@ -286,85 +399,28 @@ export default class TableList extends React.Component {
   }
 
   render() {
+    const {books, search} = this.props.App.state;
+    const {sort} = this.state;
+
+    this.books = sortBooks(
+      findMatches(books, search.query),
+      sort.column, sort.asc
+    )
+    .slice(0, 50);
+
     return (
       <div className='book-list table'>
         <section className='table-container'>
-        <table>
-          <thead>
-          <tr>{
-            this.props.data.config.bookList.table.columns.map(col =>
-              <th
-                className={this.state.sort.column == col ? 'sort-by' : ''}
-                onClick={() => this.onSort(col)}
-              >{
-                col.replace(/\b[a-z]/g, c => c.toUpperCase())
-              }</th>
-            )
-          }</tr>
-          </thead>
-
-          <tbody>{
-            sortBooks(
-              findMatches(
-                this.props.data.books, this.props.data.search.query
-              ),
-              this.state.sort.column, this.state.sort.asc
-            ).map(book =>
-              <tr
-                className={`book ${
-                  this.state.selected.indexOf(book.id) > -1
-                  ? 'selected' : ''
-                }`}
-                onClick={(e) => this.onSelect(e, book.id)}
-              >{
-                this.props.data.config.bookList.table.columns
-                .map(col => {
-                  switch (col) {
-                    case 'added': return (
-                      <td className='added'>{
-                        (new Date(book.timestamp))
-                          .toLocaleDateString()
-                      }</td>
-                    );
-
-                    case 'rating': return (
-                      <td className='rating'>{
-                        book.rating === undefined
-                        ? 'None' : (
-                          <React.Fragment>
-                            {book.rating}
-                            <FontIcon>stars</FontIcon>
-                          </React.Fragment>
-                        )
-                      }</td>
-                    );
-
-                    case 'published': return (
-                      <td className='published'>{
-                        (new Date(book.pubdate))
-                          .toLocaleDateString()
-                      }</td>
-                    );
-
-                    case 'series': return (
-                      <td className='series'>{
-                        !book.series
-                          ? ''
-                          : `${book.series} [${book.series_index}]`
-                      }</td>
-                    );
-
-                    default: return (
-                      <td className={col}>{
-                        book[col]
-                      }</td>
-                    );
-                  }
-                })
-              }</tr>
-            )
-          }</tbody>
-        </table>
+          <DataTable
+            selectableRows indeterminate responsive
+            onRowToggle={(id, checked) => this.onRowToggle(id, checked)}
+            baseId='book-list-table'
+          >
+            <TableHeader>{this._renderTableHeaderRow()}</TableHeader>
+            <TableBody>{
+              this.books.map(b => this._renderTableBodyRow(b))
+            }</TableBody>
+          </DataTable>
         </section>
 
         {this._renderSelectedBook()}
