37b018114d8c2dd027c8cce440f3527bac1eb8f2
MrXyfir <mr@xyfir.com>
2018-01-08 20:38:49 -0800

    Improvements

--- client/lib/reader/annotations/update.js
+++ client/lib/reader/annotations/update.js
@@ -5,43 +5,39 @@ import { XYANNOTATIONS_URL } from 'constants/config';
 
 /**
  * Updates the locally saved annotation sets.
+ * @async
  * @param {object[]} sets - An array of annotation sets
  * @param {string} key - Xyfir Annotations subscription key
- * @returns {Promise} A promise that always resolves to an array of updated
+ * @return {object[]} A promise that always resolves to an array of updated
  * annotation sets, or an empty array on error.
  */
-export default function(sets, key) {
+export default async function(sets, key) {
 
-  return new Promise(resolve => {
-    if (!sets || !sets.length || !key || !navigator.onLine) {
-      resolve([]);
-      return;
-    }
+  if (!sets || !sets.length || !key || !navigator.onLine) return [];
 
-    request
+  try {
+    const res = await request
       .get(`${XYANNOTATIONS_URL}/api/annotations`)
       .query({
-        subscriptionKey: key, sets: JSON.stringify(
-          sets.map(set => Object({ id: set.id, version: set.version }))
-        )
-      })
-      .end((err, res) => {
-        if (err || res.body.error) {
-          swal('Error', 'Could not update annotations', 'error');
-          resolve([]);
-        }
-        else {
-          // Check if new version has been received
-          sets.forEach((set, i) => {
-            if (res.body[set.id] && res.body[set.id].version != set.version) {
-              // Update version / items
-              Object.assign(sets[i], res.body[set.id]);
-            }
-          });
-
-          resolve(sets);
-        }
+        subscriptionKey: key,
+        sets: JSON.stringify(sets.map(s => ({id: s.id, version: s.version})))
       });
-  });
+
+    if (res.body.error) throw 'Could not update annotations';
+
+    // Check if new version has been received
+    sets.forEach((set, i) => {
+      if (res.body[set.id] && res.body[set.id].version != set.version) {
+        // Update version / items
+        Object.assign(sets[i], res.body[set.id]);
+      }
+    });
+
+    return sets;
+  }
+  catch (err) {
+    console.error('lib/reader/annotations/update', err);
+    return sets || [];
+  }
 
 }
