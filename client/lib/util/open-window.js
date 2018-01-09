99753aa5097d9f4eee466b631a120e6110a7bee8
MrXyfir <mr@xyfir.com>
2018-01-09 13:28:56 -0800

    Add client/lib/util/open-window

---
+++ client/lib/util/open-window.js
@@ -0,0 +1,12 @@
+/**
+ * Opens `url` in InAppBrowser if in a Cordova environment and via
+ *  `window.open` if in a normal browser.
+ * @param {string} url
+ * @return {Window}
+ */
+export default function(url) {
+  if (window.cordova)
+    return window.cordova.InAppBrowser.open(url, '_blank');
+  else
+    return window.open(url);
+}
