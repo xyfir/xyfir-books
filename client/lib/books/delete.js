c9df9267cfb03a61317e09aca557ab7c94c974b4
MrXyfir <mr@xyfir.com>
2018-01-12 12:14:15 -0800

    Improvements and fixes. Delete from xyLibrary instead of xyBooks

--- client/lib/books/delete.js
+++ client/lib/books/delete.js
@@ -5,48 +5,50 @@ import swal from 'sweetalert';
 import { deleteBooks } from 'actions/books';
 
 // Constants
-import { XYBOOKS_URL } from 'constants/config';
+import { XYLIBRARY_URL } from 'constants/config';
 
 /**
  * Prompts the user to confirm that they want to delete the book(s) and then
  * deletes the book(s).
+ * @async
  * @param {number[]} books
- * @param {function} dispatch
- * @return {Promise} Resolves to a boolean that is true if the books were
- * deleted.
+ * @param {object} App
+ * @return {boolean} True if the books were deleted.
  */
-export default function(books, dispatch) {
+export default async function(books, App) {
 
-  return new Promise(resolve => {
-    if (!navigator.onLine) {
-      swal(
-        'No Internet Connection',
-        'This action requires an internet connection',
-        'error'
-      );
-      return resolve(false);
-    }
+  if (!navigator.onLine) {
+    swal(
+      'No Internet Connection',
+      'This action requires an internet connection',
+      'error'
+    );
+    return false;
+  }
 
-    swal({
+  try {
+    const confirm = await swal({
       title: 'Are you sure?',
       text: `Are you sure you want to delete (${books.length}) book(s)?`,
       icon: 'warning',
       buttons: true
-    })
-    .then(() => request
-      .delete(`${XYBOOKS_URL}/api/books`)
-      .send({ ids: books.join(',') })
-    )
-    .then(res => {
-      if (res.body.error) {
-        swal('Error', 'Could not delete book(s)', 'error');
-        resolve(false);
-      }
-      else {
-        dispatch(deleteBooks(books));
-        resolve(true);
-      }
     });
-  });
+
+    if (!confirm) return false;
+
+    const res = await request
+      .delete(`${XYLIBRARY_URL}/libraries/${App.state.account.library}/books`)
+      .send({ books: books.join(',') });
+
+    if (res.body.error) throw res.body;
+
+    App.store.dispatch(deleteBooks(books));
+    return true;
+  }
+  catch (err) {
+    swal('Error', 'Could not delete book(s)', 'error');
+    console.error('lib/books/delete', err);
+    return false;
+  }
 
 }
