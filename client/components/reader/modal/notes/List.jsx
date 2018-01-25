fd1af149eb255098164860b0e55cf569cbda648e
MrXyfir <mr@xyfir.com>
2018-01-25 14:44:34 -0800

    Add `<Navigation>`.  Improvements

--- client/components/reader/modal/notes/List.jsx
+++ client/components/reader/modal/notes/List.jsx
@@ -1,6 +1,9 @@
 import { TextField, ListItem, List, Button } from 'react-md';
 import React from 'react';
 
+// Components
+import Navigation from 'components/reader/modal/Navigation';
+
 export default class NotesList extends React.Component {
 
   constructor(props) {
@@ -8,38 +11,50 @@ export default class NotesList extends React.Component {
   }
 
   render() {
-    return (
-      <div className='notes'>
-        <Button
-          raised primary
-          onClick={() => this.props.Notes.setState({ view: 'create' })}
-          iconChildren='create'
-        >Create</Button>
+    const {Notes, Reader} = this.props;
 
-        <List className='notes'>{
-          this.props.Reader.state.book.notes.map((note, i) =>
-            <ListItem
-              threeLines
-              key={note.cfi + '-' + i}
-              onClick={() =>
-                this.props.Notes.setState({ view: 'view', note: i })
-              }
-              primaryText={
-                `Created ${new Date(note.created).toLocaleString()}`
-              }
-              secondaryText={
-                (note.highlights[0].length > 50
-                  ? note.highlights[0].substr(0, 47) + '...'
-                  : note.highlights[0])
-                + '\n'
-                + (note.content.length > 50
-                  ? note.content.substr(0, 47) + '...'
-                  : note.content)
-              }
+    return (
+      <section className='notes list-container'>
+        <Navigation
+          {...this.props}
+          title='Notes'
+          actions={[
+            <Button
+              icon
+              onClick={() => Notes.setState({ view: 'create' })}
+              iconChildren='create'
             />
-          )
-        }</List>
-      </div>
+          ]}
+        />
+
+        {Reader.state.book.notes.length ? (
+          <List className='notes'>{
+            Reader.state.book.notes.map((note, i) =>
+              <ListItem
+                threeLines
+                key={note.cfi + '-' + i}
+                onClick={() =>
+                  Notes.setState({ view: 'view', note: i })
+                }
+                primaryText={
+                  `Created ${new Date(note.created).toLocaleString()}`
+                }
+                secondaryText={
+                  (note.highlights[0].length > 50
+                    ? note.highlights[0].substr(0, 47) + '...'
+                    : note.highlights[0])
+                  + '\n'
+                  + (note.content.length > 50
+                    ? note.content.substr(0, 47) + '...'
+                    : note.content)
+                }
+              />
+            )
+          }</List>
+        ) : (
+          <p>You don't have any notes!</p>
+        )}
+      </section>
     );
   }
 
