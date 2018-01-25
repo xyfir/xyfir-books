70a40e95786d21ce6f85b8d0481b53c981bf4b94
MrXyfir <mr@xyfir.com>
2018-01-25 14:43:09 -0800

    Add `<Navigation>`.  Improvements

--- client/components/reader/modal/TableOfContents.jsx
+++ client/components/reader/modal/TableOfContents.jsx
@@ -1,12 +1,15 @@
 import { ListItem, List } from 'react-md';
 import React from 'react';
 
+// Components
+import Navigation from 'components/reader/modal/Navigation';
+
 export default class TableOfContents extends React.Component {
 
   constructor(props) {
     super(props);
   }
-  
+
   /**
    * Load the selected chapter.
    * @param {string} href
@@ -18,15 +21,19 @@ export default class TableOfContents extends React.Component {
 
   render() {
     return (
-      <List className='table-of-contents'>{
-        this.props.Reader.book.navigation.toc.map(chapter =>
-          <ListItem
-            key={chapter.id}
-            onClick={() => this.onGoToChapter(chapter.href)}
-            primaryText={chapter.label.trim()}
-          />
-        )
-      }</List>
+      <section className='table-of-contents list-container'>
+        <Navigation {...this.props} title='Table of Contents' />
+
+        <List>{
+          this.props.Reader.book.navigation.toc.map(chapter =>
+            <ListItem
+              key={chapter.id}
+              onClick={() => this.onGoToChapter(chapter.href)}
+              primaryText={chapter.label.trim()}
+            />
+          )
+        }</List>
+      </section>
     );
   }
 
