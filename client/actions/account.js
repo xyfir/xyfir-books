4d6f7d9c3a38368e58937d37dbfb63c1e5dbd0c4
MrXyfir <mr@xyfir.com>
2018-02-18 13:16:30 -0800

    Remove unused actions

--- client/actions/account.js
+++ client/actions/account.js
@@ -1,19 +1,5 @@
 import * as types from 'constants/actions/account';
 
-/** @param {number} subscription */
-export function purchaseSubscription(subscription) {
-  return {
-    type: types.PURCHASE_SUBSCRIPTION, subscription
-  };
-}
-
-/** @param {number} size */
-export function setLibrarySizeLimit(size) {
-  return {
-    type: types.SET_LIBRARY_SIZE_LIMIT, size
-  };
-}
-
 /** @param {string} key */
 export function setXyAnnotationsKey(key) {
   return {
--- client/reducers/account.js
+++ client/reducers/account.js
@@ -2,16 +2,6 @@ import * as types from 'constants/actions/account';
 
 export default function(state, action) {
   switch (action.type) {
-    case types.PURCHASE_SUBSCRIPTION:
-      return Object.assign({}, state, {
-        subscription: action.subscription
-      });
-
-    case types.SET_LIBRARY_SIZE_LIMIT:
-      return Object.assign({}, state, {
-        librarySizeLimit: action.size
-      });
-
     case types.SET_XYANNOTATIONS_KEY:
       return Object.assign({}, state, {
         xyAnnotationsKey: action.key
